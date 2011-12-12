02d9bcd295a64e6b03769b33b7b9c6486c75d49c
evan.teran <evan.teran@0e5a8416-ee0f-00cc-8024-a419571235d2>
2011-12-12 21:33:51 +0000

    removed uneeded include

--- include/Debugger.h
+++ include/Debugger.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <QString>
 #include <QStringList>
-#include <QSharedPointer>
 #include <QHash>
 
 class AnalyzerInterface;
