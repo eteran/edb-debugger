6ee34c6fcb00d09129cb114f322a78e84a3aa61f
evan.teran <evan.teran@0e5a8416-ee0f-00cc-8024-a419571235d2>
2012-10-01 20:26:10 +0000

    renamed ELFBinaryInfo...

---
+++ plugins/BinaryInfo/BinaryInfo.cpp
@@ -0,0 +1,60 @@
+/*
+Copyright (C) 2006 - 2011 Evan Teran
+                          eteran@alum.rit.edu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "BinaryInfo.h"
+#include "Debugger.h"
+#include "ELF32.h"
+#include "ELF64.h"
+#include "IBinary.h"
+
+namespace {
+	IBinary *create_binary_info_elf32(const MemoryRegion &region) {
+		return new ELF32(region);
+	}
+
+	IBinary *create_binary_info_elf64(const MemoryRegion &region) {
+		return new ELF64(region);
+	}
+}
+
+//------------------------------------------------------------------------------
+// Name: BinaryInfo()
+// Desc:
+//------------------------------------------------------------------------------
+BinaryInfo::BinaryInfo() {
+}
+
+//------------------------------------------------------------------------------
+// Name: BinaryInfo()
+// Desc:
+//------------------------------------------------------------------------------
+void BinaryInfo::private_init() {
+	edb::v1::register_binary_info(create_binary_info_elf32);
+	edb::v1::register_binary_info(create_binary_info_elf64);
+}
+
+//------------------------------------------------------------------------------
+// Name: menu(QWidget *parent)
+// Desc:
+//------------------------------------------------------------------------------
+QMenu *BinaryInfo::menu(QWidget *parent) {
+	Q_UNUSED(parent);
+	return 0;
+}
+
+Q_EXPORT_PLUGIN2(BinaryInfo, BinaryInfo)
---
+++ plugins/BinaryInfo/BinaryInfo.pro
@@ -0,0 +1,8 @@
+
+include(../plugins.pri)
+
+# Input
+HEADERS += BinaryInfo.h ELF32.h ELF64.h
+FORMS += 
+SOURCES += BinaryInfo.cpp ELF32.cpp ELF64.cpp
+
