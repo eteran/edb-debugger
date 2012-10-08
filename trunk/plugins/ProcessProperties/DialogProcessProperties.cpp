c6a92d586b7210dbcb383c661ee5960c4f076cf2
evan.teran <evan.teran@0e5a8416-ee0f-00cc-8024-a419571235d2>
2012-10-08 20:01:49 +0000

    added ability to view environment on Linux

--- trunk/plugins/ProcessProperties/DialogProcessProperties.cpp
+++ trunk/plugins/ProcessProperties/DialogProcessProperties.cpp
@@ -17,14 +17,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "DialogProcessProperties.h"
-#include "IDebuggerCore.h"
 #include "Debugger.h"
+#include "IDebuggerCore.h"
 #include "MemoryRegions.h"
-#include <QStringList>
+#include "Configuration.h"
+#include "ISymbolManager.h"
+#include "Symbol.h"
 #include <QDebug>
 #include <QDesktopServices>
-#include <QFileInfo>
 #include <QDir>
+#include <QFileInfo>
+#include <QSortFilterProxyModel>
+#include <QStringList>
+#include <QStringListModel>
 #include <QUrl>
 
 #if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD) || defined(Q_OS_OPENBSD)
@@ -58,6 +63,15 @@ DialogProcessProperties::DialogProcessProperties(QWidget *parent) : QDialog(pare
 	ui->setupUi(this);
 	ui->tableModules->horizontalHeader()->setResizeMode(QHeaderView::ResizeToContents);
 	ui->tableMemory->horizontalHeader()->setResizeMode(QHeaderView::ResizeToContents);
+	
+	model_        = new QStringListModel(this);
+	filter_model_ = new QSortFilterProxyModel(this);
+	
+	/*
+	filter_model_->setFilterKeyColumn(0);
+	filter_model_->setSourceModel(model_);
+	ui->tableEnvironment->setModel(filter_model_);
+	*/
 }
 
 //------------------------------------------------------------------------------
@@ -121,28 +135,76 @@ void DialogProcessProperties::updateModulePage() {
 //------------------------------------------------------------------------------
 void DialogProcessProperties::updateMemoryPage() {
 
-		ui->tableMemory->clearContents();
-		ui->tableMemory->setRowCount(0);
+	ui->tableMemory->clearContents();
+	ui->tableMemory->setRowCount(0);
 
-		if(edb::v1::debugger_core) {
-			edb::v1::memory_regions().sync();
-			const QList<MemoryRegion> regions = edb::v1::memory_regions().regions();
-			ui->tableMemory->setSortingEnabled(false);
+	if(edb::v1::debugger_core) {
+		edb::v1::memory_regions().sync();
+		const QList<MemoryRegion> regions = edb::v1::memory_regions().regions();
+		ui->tableMemory->setSortingEnabled(false);
 
 
-			Q_FOREACH(const MemoryRegion &r, regions) {
-				const int row = ui->tableMemory->rowCount();
-				ui->tableMemory->insertRow(row);
-				ui->tableMemory->setItem(row, 0, new QTableWidgetItem(edb::v1::format_pointer(r.start()))); // address
-				ui->tableMemory->setItem(row, 1, new QTableWidgetItem(size_to_string(r.size())));           // size
-				ui->tableMemory->setItem(row, 2, new QTableWidgetItem(QString("%1%2%3")                     // protection
-					.arg(r.readable() ? 'r' : '-')
-					.arg(r.writable() ? 'w' : '-')
-					.arg(r.executable() ? 'x' : '-'))); 
-				ui->tableMemory->setItem(row, 3, new QTableWidgetItem(r.name()));                           // name
-			}
-			ui->tableMemory->setSortingEnabled(true);
+		Q_FOREACH(const MemoryRegion &r, regions) {
+			const int row = ui->tableMemory->rowCount();
+			ui->tableMemory->insertRow(row);
+			ui->tableMemory->setItem(row, 0, new QTableWidgetItem(edb::v1::format_pointer(r.start()))); // address
+			ui->tableMemory->setItem(row, 1, new QTableWidgetItem(size_to_string(r.size())));           // size
+			ui->tableMemory->setItem(row, 2, new QTableWidgetItem(QString("%1%2%3")                     // protection
+				.arg(r.readable() ? 'r' : '-')
+				.arg(r.writable() ? 'w' : '-')
+				.arg(r.executable() ? 'x' : '-'))); 
+			ui->tableMemory->setItem(row, 3, new QTableWidgetItem(r.name()));                           // name
 		}
+		ui->tableMemory->setSortingEnabled(true);
+	}
+}
+
+//------------------------------------------------------------------------------
+// Name: 
+// Desc:
+//------------------------------------------------------------------------------
+void DialogProcessProperties::on_txtSearchEnvironment_textChanged(const QString &text) {
+	updateEnvironmentPage(text);
+}
+
+//------------------------------------------------------------------------------
+// Name: 
+// Desc:
+//------------------------------------------------------------------------------
+void DialogProcessProperties::updateEnvironmentPage(const QString &filter) {
+	// tableEnvironment
+	
+	ui->tableEnvironment->clearContents();
+	ui->tableEnvironment->setSortingEnabled(false);
+	ui->tableEnvironment->setRowCount(0);
+	
+	const QString lower_filter = filter.toLower();
+	
+#ifdef Q_OS_LINUX
+	QFile proc_environ(QString("/proc/%1/environ").arg(edb::v1::debugger_core->pid()));
+	if(proc_environ.open(QIODevice::ReadOnly)) {
+		QByteArray env = proc_environ.readAll();
+		char *p = env.data();
+		char *ptr = p;
+		while(ptr != p + env.size()) {
+			const QString env = QString::fromUtf8(ptr);
+			const QString env_name  = env.mid(0, env.indexOf("="));
+			const QString env_value = env.mid(env.indexOf("=") + 1);
+
+			if(lower_filter.isEmpty() || env_name.toLower().contains(lower_filter)) {
+				const int row = ui->tableEnvironment->rowCount();
+				ui->tableEnvironment->insertRow(row);
+				ui->tableEnvironment->setItem(row, 0, new QTableWidgetItem(env_name));
+			    ui->tableEnvironment->setItem(row, 1, new QTableWidgetItem(env_value));
+			}
+			
+			ptr += qstrlen(ptr) + 1;
+		}
+
+	}
+#endif
+
+	ui->tableEnvironment->setSortingEnabled(true);
 }
 
 //------------------------------------------------------------------------------
@@ -153,6 +215,7 @@ void DialogProcessProperties::showEvent(QShowEvent *) {
 	updateGeneralPage();
 	updateMemoryPage();
 	updateModulePage();
+	updateEnvironmentPage(ui->txtSearchEnvironment->text());
 }
 
 //------------------------------------------------------------------------------
@@ -181,4 +244,12 @@ void DialogProcessProperties::on_btnImage_clicked() {
 		QDir dir = info.absoluteDir();
 		QDesktopServices::openUrl(QUrl(QString("file:///%1").arg(dir.absolutePath()), QUrl::TolerantMode));
 	}
+}
+
+//------------------------------------------------------------------------------
+// Name: 
+// Desc:
+//------------------------------------------------------------------------------
+void DialogProcessProperties::on_btnRefreshEnvironment_clicked() {
+	updateEnvironmentPage(ui->txtSearchEnvironment->text());
 }
