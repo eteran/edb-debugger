6e6ac239a653716529d91de3a66d5c236350f786
evan.teran <evan.teran@0e5a8416-ee0f-00cc-8024-a419571235d2>
2012-10-10 02:14:04 +0000

    starting to add PE parsing..

---
+++ plugins/BinaryInfo/PE32.h
@@ -0,0 +1,39 @@
+/*
+Copyright (C) 2006 - 2011 Evan Teran
+                          eteran@alum.rit.edu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef PE32_20070718_H_
+#define PE32_20070718_H_
+
+#include "IBinary.h"
+
+class PE32 : public IBinary {
+public:
+	PE32(const MemoryRegion &region);
+	virtual ~PE32();
+
+public:
+	virtual bool validate_header();
+	virtual edb::address_t entry_point();
+	virtual edb::address_t calculate_main();
+	virtual bool native() const;
+	virtual edb::address_t debug_pointer();
+	virtual size_t header_size() const;
+};
+
+#endif
+
---
+++ plugins/BinaryInfo/pe_binary.h
@@ -0,0 +1,147 @@
+/*
+Copyright (C) 2012 Evan Teran
+                   eteran@alum.rit.edu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef PE_BINARY_20121007_H_
+#define PE_BINARY_20121007_H_
+
+#include <stdint.h>
+
+typedef uint8_t  BYTE;
+typedef uint16_t WORD;
+typedef uint32_t DWORD;
+typedef uint64_t ULONGLONG;
+typedef int32_t  LONG;
+
+#define IMAGE_NUMBEROF_DIRECTORY_ENTRIES    16
+
+typedef struct _IMAGE_DOS_HEADER {
+	WORD e_magic; // "MZ"
+	WORD e_cblp;
+	WORD e_cp;
+	WORD e_crlc;
+	WORD e_cparhdr;
+	WORD e_minalloc;
+	WORD e_maxalloc;
+	WORD e_ss;
+	WORD e_sp;
+	WORD e_csum;
+	WORD e_ip;
+	WORD e_cs;
+	WORD e_lfarlc;
+	WORD e_ovno;
+	WORD e_res[4];
+	WORD e_oemid;
+	WORD e_oeminfo;
+	WORD e_res2[10];
+	LONG e_lfanew;
+} IMAGE_DOS_HEADER, *PIMAGE_DOS_HEADER;
+
+typedef struct _IMAGE_DATA_DIRECTORY {
+	DWORD VirtualAddress;
+	DWORD Size;
+} IMAGE_DATA_DIRECTORY, *PIMAGE_DATA_DIRECTORY;
+
+typedef struct _IMAGE_OPTIONAL_HEADER64 {
+	WORD        Magic;
+	BYTE        MajorLinkerVersion;
+	BYTE        MinorLinkerVersion;
+	DWORD       SizeOfCode;
+	DWORD       SizeOfInitializedData;
+	DWORD       SizeOfUninitializedData;
+	DWORD       AddressOfEntryPoint;
+	DWORD       BaseOfCode;
+	ULONGLONG   ImageBase;
+	DWORD       SectionAlignment;
+	DWORD       FileAlignment;
+	WORD        MajorOperatingSystemVersion;
+	WORD        MinorOperatingSystemVersion;
+	WORD        MajorImageVersion;
+	WORD        MinorImageVersion;
+	WORD        MajorSubsystemVersion;
+	WORD        MinorSubsystemVersion;
+	DWORD       Win32VersionValue;
+	DWORD       SizeOfImage;
+	DWORD       SizeOfHeaders;
+	DWORD       CheckSum;
+	WORD        Subsystem;
+	WORD        DllCharacteristics;
+	ULONGLONG   SizeOfStackReserve;
+	ULONGLONG   SizeOfStackCommit;
+	ULONGLONG   SizeOfHeapReserve;
+	ULONGLONG   SizeOfHeapCommit;
+	DWORD       LoaderFlags;
+	DWORD       NumberOfRvaAndSizes;
+	IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+} IMAGE_OPTIONAL_HEADER64, *PIMAGE_OPTIONAL_HEADER64;
+
+typedef struct _IMAGE_OPTIONAL_HEADER32 {
+	WORD                 Magic;
+	BYTE                 MajorLinkerVersion;
+	BYTE                 MinorLinkerVersion;
+	DWORD                SizeOfCode;
+	DWORD                SizeOfInitializedData;
+	DWORD                SizeOfUninitializedData;
+	DWORD                AddressOfEntryPoint;
+	DWORD                BaseOfCode;
+	DWORD                BaseOfData;
+	DWORD                ImageBase;
+	DWORD                SectionAlignment;
+	DWORD                FileAlignment;
+	WORD                 MajorOperatingSystemVersion;
+	WORD                 MinorOperatingSystemVersion;
+	WORD                 MajorImageVersion;
+	WORD                 MinorImageVersion;
+	WORD                 MajorSubsystemVersion;
+	WORD                 MinorSubsystemVersion;
+	DWORD                Win32VersionValue;
+	DWORD                SizeOfImage;
+	DWORD                SizeOfHeaders;
+	DWORD                CheckSum;
+	WORD                 Subsystem;
+	WORD                 DllCharacteristics;
+	DWORD                SizeOfStackReserve;
+	DWORD                SizeOfStackCommit;
+	DWORD                SizeOfHeapReserve;
+	DWORD                SizeOfHeapCommit;
+	DWORD                LoaderFlags;
+	DWORD                NumberOfRvaAndSizes;
+	IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+} IMAGE_OPTIONAL_HEADER32, *PIMAGE_OPTIONAL_HEADER32;
+
+typedef struct _IMAGE_FILE_HEADER {
+	WORD  Machine;
+	WORD  NumberOfSections;
+	DWORD TimeDateStamp;
+	DWORD PointerToSymbolTable;
+	DWORD NumberOfSymbols;
+	WORD  SizeOfOptionalHeader;
+	WORD  Characteristics;
+} IMAGE_FILE_HEADER, *PIMAGE_FILE_HEADER;
+
+typedef struct _IMAGE_NT_HEADERS32 {
+	DWORD                 Signature;
+	IMAGE_FILE_HEADER     FileHeader;
+	IMAGE_OPTIONAL_HEADER32 OptionalHeader;
+} IMAGE_NT_HEADERS32, *PIMAGE_NT_HEADERS32;
+
+typedef struct _IMAGE_NT_HEADERS64 {
+	DWORD Signature;
+	IMAGE_FILE_HEADER FileHeader;
+	IMAGE_OPTIONAL_HEADER64 OptionalHeader;
+} IMAGE_NT_HEADERS64, *PIMAGE_NT_HEADERS64;
+#endif
