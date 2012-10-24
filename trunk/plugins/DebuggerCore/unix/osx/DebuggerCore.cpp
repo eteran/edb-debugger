700f88167ef91310a1feda5b49138121d8cfab2a
evan.teran@gmail.com <evan.teran@gmail.com@0e5a8416-ee0f-00cc-8024-a419571235d2>
2012-10-24 16:15:42 +0000

    fixed compilation on osx

--- trunk/plugins/DebuggerCore/unix/osx/DebuggerCore.cpp
+++ trunk/plugins/DebuggerCore/unix/osx/DebuggerCore.cpp
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "PlatformRegion.h"
 #include "PlatformState.h"
 #include "State.h"
+#include <QDebug>
 
 #include <fcntl.h>
 #include <mach/mach.h>
