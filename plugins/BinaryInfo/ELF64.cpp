2a200e85149e492ecaac0603e7a81cf6e08b4c5f
evan.teran@gmail.com <evan.teran@gmail.com@0e5a8416-ee0f-00cc-8024-a419571235d2>
2012-10-24 16:15:42 +0000

    fixed compilation on osx

--- plugins/BinaryInfo/ELF64.cpp
+++ plugins/BinaryInfo/ELF64.cpp
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QDebug>
 #include <cstring>
 
-#if defined(Q_OS_UNIX)
+#if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD) || defined(Q_OS_OPENBSD)
 #include <link.h>
 #endif
 
