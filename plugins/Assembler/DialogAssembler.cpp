ccb62e7e773fc0751c5bfa2ac27b2ecbb5595456
evan.teran@gmail.com <evan.teran@gmail.com@0e5a8416-ee0f-00cc-8024-a419571235d2>
2013-06-18 20:56:53 +0000

    added necessary includes

--- plugins/Assembler/DialogAssembler.cpp
+++ plugins/Assembler/DialogAssembler.cpp
@@ -17,8 +17,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "DialogAssembler.h"
-#include "edb.h"
 #include "IDebuggerCore.h"
+#include "edb.h"
+#include "string_hash.h"
 
 #include <QMessageBox>
 #include <QDebug>
