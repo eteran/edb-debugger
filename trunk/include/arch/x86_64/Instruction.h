5a380ccf77f84ef21d0fac4666cf5d8fa16b0f7e
evan.teran@gmail.com <evan.teran@gmail.com@0e5a8416-ee0f-00cc-8024-a419571235d2>
2014-01-08 03:43:51 +0000

    updating to use edisassm 1.8.0 API

--- trunk/include/arch/x86_64/Instruction.h
+++ trunk/include/arch/x86_64/Instruction.h
@@ -16,4 +16,4 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "../../../src/edisassm/instruction.h"
+#include "../../../src/edisassm/Instruction.h"
