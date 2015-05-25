7c87ba956f8fa79eccbf4852864c5ddf5b17c25c
Northern-Lights <aboursalian@aol.com>
2015-05-24 18:45:27 -0700

    Added licensing info

--- plugins/Backtrace/DialogBacktrace.cpp
+++ plugins/Backtrace/DialogBacktrace.cpp
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2015	Armen Boursalian
+					aboursalian@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "DialogBacktrace.h"
 #include "ui_DialogBacktrace.h"
 #include "CallStack.h"
@@ -7,8 +25,6 @@
 
 #include <QTableWidget>
 #include <QMessageBox>
-
-#include <QDebug>
 
 //------------------------------------------------------------------------------
 // Name: DialogBacktrace
