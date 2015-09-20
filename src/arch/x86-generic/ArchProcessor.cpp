502c3824e7e2d6c4266dd67ae955d2b65429f032
Ruslan Kabatsayev <b7.10110111@gmail.com>
2015-09-20 20:17:43 +0300

    Don't read segment bases as reg_t, since they may be smaller than reg_t and return garbage in upper part

--- src/arch/x86-generic/ArchProcessor.cpp
+++ src/arch/x86-generic/ArchProcessor.cpp
@@ -1018,7 +1018,7 @@ void ArchProcessor::update_register_view(const QString &default_region_name, con
 		QString sreg(specialSegs[i]);
 		QString sregStr=sreg.toUpper()+QString(": %1").arg(state[sreg].value<edb::seg_reg_t>().toHexString());
 		if(bases[i])
-			sregStr+=QString(" (%1)").arg(bases[i].value<edb::reg_t>().toHexString());
+			sregStr+=QString(" (%1)").arg(bases[i].valueAsAddress().toHexString());
 		register_view_items_[itemNumber]->setText(0, sregStr);
 		register_view_items_[itemNumber++]->setForeground(0, QBrush((state[sreg] != last_state_[sreg]) ? Qt::red : palette.text()));
 	}
