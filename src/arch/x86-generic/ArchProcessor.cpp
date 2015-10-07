895abe60ef8c7b194d50c811f10871f0e2ab8269
Ruslan Kabatsayev <b7.10110111@gmail.com>
2015-10-07 18:47:45 +0300

    Remove unused include

--- src/arch/x86-generic/ArchProcessor.cpp
+++ src/arch/x86-generic/ArchProcessor.cpp
@@ -35,7 +35,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QVector>
 #include <QXmlQuery>
 
-#include <boost/math/special_functions/fpclassify.hpp>
 #include <climits>
 #include <cmath>
 
