cf4512afd295794dbf13a9ff4ab77d83967abc04
Evan Teran <evan.teran@gmail.com>
2015-10-10 13:42:48 -0400

    why is this impossible to get right?

--- travis_install_capstone.sh
+++ travis_install_capstone.sh
@@ -3,7 +3,7 @@
 set -ex
 mkdir -p $HOME/src
 cd $HOME/src
-git clone --depth=50 --branch=3.0.4 https://github.com/aquynj/capstone.git
+git clone --depth=50 --branch=3.0.4 https://github.com/aquynh/capstone.git
 cd capstone
 ./make.sh
 sudo ./make.sh install
