7d9fd072a6b75de29afbfa66ce80d38e02d767ba
Ruslan Kabatsayev <b7.10110111@gmail.com>
2015-11-19 14:16:42 +0300

    Simplify logic of writing byte

--- plugins/DebuggerCore/unix/linux/PlatformProcess.cpp
+++ plugins/DebuggerCore/unix/linux/PlatformProcess.cpp
@@ -514,38 +514,24 @@ void PlatformProcess::write_byte(edb::address_t address, quint8 value, bool *ok)
 
 	*ok = false;
 
-	long v;
-	long mask;
 	// core_->page_size() - 1 will always be 0xf* because pagesizes
 	// are always 0x10*, so the masking works
-	// range of a is [1..n] where n=pagesize, and we have to adjust
-	// if a < wordsize
-	const edb::address_t a = core_->page_size() - (address & (core_->page_size() - 1));
+	// range of nBytesToNextPage is [1..n] where n=pagesize, and we have to adjust
+	// if nBytesToNextPage < wordsize
+	const edb::address_t nBytesToNextPage = core_->page_size() - (address & (core_->page_size() - 1));
 
-	v = value;
-#if Q_BYTE_ORDER == Q_LITTLE_ENDIAN
-	if(a < EDB_WORDSIZE) {
-		address -= (EDB_WORDSIZE - a);                       // LE + BE
-		mask = ~(0xffUL << (CHAR_BIT * (EDB_WORDSIZE - a))); // LE
-		v <<= CHAR_BIT * (EDB_WORDSIZE - a);                 // LE
-	} else {
-		mask = ~0xffUL; // LE
-	}
-#else /* BIG ENDIAN */
-	if(a < EDB_WORDSIZE) {
-		address -= (EDB_WORDSIZE - a);            // LE + BE
-		mask = ~(0xffUL << (CHAR_BIT * (a - 1))); // BE
-		v <<= CHAR_BIT * (a - 1);                 // BE
-	} else {
-		mask = ~(0xffUL << (CHAR_BIT * (EDB_WORDSIZE - 1))); // BE
-		v <<= CHAR_BIT * (EDB_WORDSIZE - 1);                 // BE
-	}
-#endif
+	// Avoid crossing page boundary, since next page may be inaccessible
+	const edb::address_t addressShift = nBytesToNextPage < EDB_WORDSIZE ? EDB_WORDSIZE - nBytesToNextPage : 0;
+	address -= addressShift;
 
-	v |= (read_data(address, ok) & mask);
-	if(*ok) {
-		*ok = write_data(address, v);
-	}
+	long word = read_data(address, ok);
+	if(!*ok) return;
+
+	// We aren't interested in `value` as in number, it's just a buffer, so no endianness magic.
+	// Just have to compensate for `addressShift` when writing it.
+	std::memcpy(reinterpret_cast<char*>(&word)+addressShift,&value,sizeof value);
+
+	*ok = write_data(address, word);
 }
 
 //------------------------------------------------------------------------------
