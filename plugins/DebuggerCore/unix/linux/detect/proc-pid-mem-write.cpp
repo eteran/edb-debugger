96e7aca3351cae9b2271f5be515494cc8ece78a3
Evan Teran <evan.teran@gmail.com>
2015-12-24 14:29:57 -0500

    fixed an issue where the sub-process was never killed! so we had tons of processes just sitting around after many builds.
    also did some misc cleanup of the code, not major.

--- plugins/DebuggerCore/unix/linux/detect/proc-pid-mem-write.cpp
+++ plugins/DebuggerCore/unix/linux/detect/proc-pid-mem-write.cpp
@@ -1,17 +1,17 @@
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <fcntl.h>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <time.h>
-#include <fcntl.h>
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <cstddef>
-#include <cstdlib>
 
 int main(int argc, char** argv);
 
@@ -27,13 +27,13 @@ public:
         fd=open(filename.c_str(),O_RDWR);
         success=fd!=-1;
     }
-    ssize_t write(const char* buf, size_t count)
+    ssize_t write(const void* buf, size_t count)
     {
         const auto result=::write(fd,buf,count);
         success=result!=-1;
         return result;
     }
-    ssize_t read(char* buf, size_t count)
+    ssize_t read(void* buf, size_t count)
     {
         const auto result=::read(fd,buf,count);
         success=result!=-1;
@@ -54,108 +54,116 @@ public:
 static const char* headerName=nullptr;
 void writeHeader(bool broken)
 {
-	std::ofstream file(headerName);
-	file << "#define PROC_PID_MEM_WRITE_BROKEN " << std::boolalpha << broken << "\n";
+    std::ofstream file(headerName);
+    file << "#define PROC_PID_MEM_WRITE_BROKEN " << std::boolalpha << broken << "\n";
 }
 
 bool detectAndWriteHeader(std::string progName)
 {
-    pid_t pid=fork();
-    if(!pid)
-    {
+    switch(pid_t pid = fork()) {
+    case 0:
         if(ptrace(PTRACE_TRACEME)<0)
         {
             perror((progName+": child: PTRACE_TRACEME failed").c_str());
-			abort();
+            abort();
         }
 
         execl(progName.c_str(),progName.c_str(),"/dev/null","child",nullptr);
         perror((progName+"child: execl() returned").c_str());
         abort();
-    }
-    else
-    {
-        int status;
-        if(waitpid(pid,&status,__WALL)==-1)
+		
+    case -1:
+        perror("fork");
+        return false;
+		
+    default:    
         {
-            perror((progName+": parent: waitpid failed").c_str());
-            return false;
-        }
-
-        if(!WIFSTOPPED(status)||WSTOPSIG(status)!=SIGTRAP)
-        {
-            std::cerr << progName << ": unexpected status returned by waitpid: 0x"
-					  << std::hex << status << "\n";
-            return false;
-        }
-
-        File file("/proc/"+std::to_string(pid)+"/mem");
-        if(!file)
-        {
-            perror((progName+": failed to open memory file").c_str());
-            return false;
-        }
-
-        const auto pageAlignMask=~(sysconf(_SC_PAGESIZE)-1);
-        const auto addr=reinterpret_cast<std::size_t>(&main) & pageAlignMask;
-        file.seekp(addr);
-        if(!file)
-        {
-            perror((progName+": failed to seek to address to read").c_str());
-            return false;
-        }
-
-        int buf=0x12345678;
-        const size_t len=sizeof buf;
-        {
-            file.read(reinterpret_cast<char*>(&buf),len);
-            if(!file)
+            int status;
+            if(waitpid(pid,&status,__WALL)==-1)
             {
-                perror((progName+": failed to read data from child's memory, won't even try to write").c_str());
+                perror((progName+": parent: waitpid failed").c_str());
                 return false;
             }
-        }
 
-        file.seekp(addr);
-        if(!file)
-        {
-            perror((progName+": failed to seek to address to write").c_str());
-            return false;
-        }
+            if(!WIFSTOPPED(status)||WSTOPSIG(status)!=SIGTRAP)
+            {
+                std::cerr << progName << ": unexpected status returned by waitpid: 0x"
+                          << std::hex << status << "\n";
+                return false;
+            }
 
-        {
-            file.write(reinterpret_cast<char*>(&buf),len);
-            if(!file) writeHeader(true);
-            else writeHeader(false);
-			return true;
-        }
+            File file("/proc/"+std::to_string(pid)+"/mem");
+            if(!file)
+            {
+                perror((progName+": failed to open memory file").c_str());
+                return false;
+            }
 
-        if(kill(pid,SIGKILL)==-1)
-        {
-            perror((progName+": warning: failed to kill child").c_str());
-            return false;
+            const auto pageAlignMask=~(sysconf(_SC_PAGESIZE)-1);
+            const auto addr=reinterpret_cast<std::size_t>(&main) & pageAlignMask;
+            file.seekp(addr);
+            if(!file)
+            {
+                perror((progName+": failed to seek to address to read").c_str());
+                return false;
+            }
+
+            int buf=0x12345678;
+            {
+                file.read(&buf,sizeof(buf));
+                if(!file)
+                {
+                    perror((progName+": failed to read data from child's memory, won't even try to write").c_str());
+                    return false;
+                }
+            }
+
+            file.seekp(addr);
+            if(!file)
+            {
+                perror((progName+": failed to seek to address to write").c_str());
+                return false;
+            }
+
+            {
+                file.write(&buf,sizeof(buf));
+                if(!file) {
+                    writeHeader(true);
+                } else {
+                    writeHeader(false);
+                }
+            }
+
+            if(kill(pid,SIGKILL)==-1)
+            {
+                perror((progName+": warning: failed to kill child").c_str());
+                return false;
+            }
+
+            return true;
         }
-	}
+        break;
+    }
 }
 
 int main(int argc, char** argv)
 {
-	if(argc>2)
-	{
-		for(;;) sleep(10);
-		abort();
-	}
+    if(argc>2)
+    {
+        for(;;) sleep(10);
+        abort();
+    }
 
-	if(argc!=2)
-	{
-		std::cerr << "Usage: " << argv[0] << " headerFileName.h\n";
-		return 1;
-	}
-	headerName=argv[1];
+    if(argc!=2)
+    {
+        std::cerr << "Usage: " << argv[0] << " headerFileName.h\n";
+        return 1;
+    }
+    headerName=argv[1];
 
-	if(!detectAndWriteHeader(argv[0]))
-	{
-		std::cerr << "Warning: failed to detect whether write to /proc/PID/mem is broken. Assuming it's not.\n";
-		writeHeader(false);
-	}
+    if(!detectAndWriteHeader(argv[0]))
+    {
+        std::cerr << "Warning: failed to detect whether write to /proc/PID/mem is broken. Assuming it's not.\n";
+        writeHeader(false);
+    }
 }
