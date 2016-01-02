061503ca3dcf0e0a040e8072e0e1c04d12967382
Ruslan Kabatsayev <b7.10110111@gmail.com>
2016-01-02 22:43:53 +0300

    Fix whitespace

--- plugins/ODbgRegisterView/RegisterView.h
+++ plugins/ODbgRegisterView/RegisterView.h
@@ -56,27 +56,27 @@ class ODBRegView : public QScrollArea
 	std::vector<RegisterGroupType> regGroupTypes;
 	void addGroup(RegisterGroupType type);
 public:
-    ODBRegView(QWidget* parent=nullptr);
+	ODBRegView(QWidget* parent=nullptr);
 	void setModel(QAbstractItemModel* model);
-    QList<ValueField*> valueFields() const;
-    QList<FieldWidget*> fields() const;
+	QList<ValueField*> valueFields() const;
+	QList<FieldWidget*> fields() const;
 private:
 	ValueField* selectedField() const;
-    void updateFieldsPalette();
-    void focusOutEvent(QFocusEvent*) override;
-    void focusInEvent(QFocusEvent*) override;
-    void keyPressEvent(QKeyEvent* event) override;
+	void updateFieldsPalette();
+	void focusOutEvent(QFocusEvent*) override;
+	void focusInEvent(QFocusEvent*) override;
+	void keyPressEvent(QKeyEvent* event) override;
 
 	QList<RegisterGroup*> groups;
 private Q_SLOTS:
-    void fieldSelected();
+	void fieldSelected();
 	void modelReset();
 	void modelUpdated();
 };
 
 class FieldWidget : public QLabel
 {
-    Q_OBJECT
+	Q_OBJECT
 
 	void init(int fieldWidth);
 protected:
@@ -84,8 +84,8 @@ protected:
 	virtual QString text() const;
 	ODBRegView* regView() const;
 public:
-    FieldWidget(int fieldWidth,QModelIndex const& index,QWidget* parent=nullptr);
-    FieldWidget(int fieldWidth,QString const& fixedText,QWidget* parent=nullptr);
+	FieldWidget(int fieldWidth,QModelIndex const& index,QWidget* parent=nullptr);
+	FieldWidget(int fieldWidth,QString const& fixedText,QWidget* parent=nullptr);
 public Q_SLOTS:
 	virtual void update();
 };
@@ -94,59 +94,59 @@ class ValueField : public FieldWidget
 {
 	Q_OBJECT
 
-    bool selected_=false;
-    bool hovered_=false;
+	bool selected_=false;
+	bool hovered_=false;
 	std::function<QString(QString)> valueFormatter;
 
 	void init();
 	bool changed() const;
-    QColor fgColorForChangedField() const;
+	QColor fgColorForChangedField() const;
 protected:
 	QString text() const override;
-    void enterEvent(QEvent*) override;
-    void leaveEvent(QEvent*) override;
-    void mousePressEvent(QMouseEvent* event) override;
-    void mouseDoubleClickEvent(QMouseEvent* event) override;
-    void paintEvent(QPaintEvent* event) override;
+	void enterEvent(QEvent*) override;
+	void leaveEvent(QEvent*) override;
+	void mousePressEvent(QMouseEvent* event) override;
+	void mouseDoubleClickEvent(QMouseEvent* event) override;
+	void paintEvent(QPaintEvent* event) override;
 
 	ValueField* bestNeighbor(std::function<bool(QPoint const& neighborPos,
 												ValueField const*curResult,
 												QPoint const& selfPos)>const& firstIsBetter) const;
 public:
-    ValueField(int fieldWidth,
+	ValueField(int fieldWidth,
 			   QModelIndex const& index,
 			   QWidget* parent=nullptr,
 			   std::function<QString(QString const&)> const& valueFormatter=[](QString const&s){return s;}
 			   );
-    ValueField* up() const;
-    ValueField* down() const;
-    ValueField* left() const;
-    ValueField* right() const;
+	ValueField* up() const;
+	ValueField* down() const;
+	ValueField* left() const;
+	ValueField* right() const;
 
-    bool isSelected() const;
+	bool isSelected() const;
 public Q_SLOTS:
 	void update() override;
-    void select();
-    void unselect();
-    void updatePalette();
+	void select();
+	void unselect();
+	void updatePalette();
 Q_SIGNALS:
-    void selected();
+	void selected();
 };
 
 class RegisterGroup : public QWidget
 {
-    Q_OBJECT
+	Q_OBJECT
 
 	int lineAfterLastField() const;
 public:
-    RegisterGroup(QWidget* parent=nullptr);
-    QList<FieldWidget*> fields() const;
-    QList<ValueField*> valueFields() const;
+	RegisterGroup(QWidget* parent=nullptr);
+	QList<FieldWidget*> fields() const;
+	QList<ValueField*> valueFields() const;
 	void setIndices(QList<QModelIndex> const& indices);
-    void insert(int line, int column, FieldWidget* widget);
+	void insert(int line, int column, FieldWidget* widget);
 	void appendNameValueComment(QModelIndex const& nameIndex,bool insertComment=true);
 protected:
-    void mousePressEvent(QMouseEvent* event);
+	void mousePressEvent(QMouseEvent* event);
 public Q_SLOTS:
 	void adjustWidth();
 };
