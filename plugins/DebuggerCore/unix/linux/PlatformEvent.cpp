56c8eb59ad15c451344810e41b2aee7ad08570be
Ruslan Kabatsayev <b7.10110111@gmail.com>
2016-01-19 22:23:09 +0300

    Deduplicate message

--- plugins/DebuggerCore/unix/linux/PlatformEvent.cpp
+++ plugins/DebuggerCore/unix/linux/PlatformEvent.cpp
@@ -41,9 +41,7 @@ IDebugEvent *PlatformEvent::clone() const {
 IDebugEvent::Message PlatformEvent::createUnexpectedSignalMessage(const QString &name, int number) {
 	return Message(
 		tr("Unexpected Signal Encountered"),
-		tr(
-			"<p>The debugged application encountered a %1 (%2).</p>"
-			"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>").arg(name).arg(number)
+		tr("<p>The debugged application encountered a %1 (%2).</p>").arg(name).arg(number)
 		);
 }
 
@@ -57,214 +55,228 @@ IDebugEvent::Message PlatformEvent::error_description() const {
 
 	std::size_t debuggeePtrSize=edb::v1::pointer_size();
 	bool fullAddressKnown=debuggeePtrSize<=sizeof(void*);
+	const auto addressString=fault_address.toPointerString(fullAddressKnown);
 
+	Message message;
 	switch(code()) {
 	case SIGSEGV:
 		switch(siginfo_.si_code) {
 		case SEGV_MAPERR:
-			return Message(
+			message=Message(
 				tr("Illegal Access Fault"),
-				tr(
-					"<p>The debugged application encountered a segmentation fault.<br />The address <strong>%1</strong> does not appear to be mapped.</p>"
-					"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>").arg(fault_address.toPointerString(fullAddressKnown))
+				tr("<p>The debugged application encountered a segmentation fault.<br />The address <strong>%1</strong> does not appear to be mapped.</p>").arg(addressString)
 				);
+			break;
 		case SEGV_ACCERR:
-			return Message(
+			message=Message(
 				tr("Illegal Access Fault"),
-				tr(
-					"<p>The debugged application encountered a segmentation fault.<br />The address <strong>%1</strong> could not be accessed.</p>"
-					"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>").arg(fault_address.toPointerString(fullAddressKnown))
+				tr("<p>The debugged application encountered a segmentation fault.<br />The address <strong>%1</strong> could not be accessed.</p>").arg(addressString)
 				);
+			break;
 		default:
-			return Message(
+			message=Message(
 				tr("Illegal Access Fault"),
-				tr(
-					"<p>The debugged application encountered a segmentation fault.<br />The instruction could not be executed.</p>"
-					"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>")
+				tr("<p>The debugged application encountered a segmentation fault.<br />The instruction could not be executed.</p>")
 				);
+			break;
 		}
+		break;
 
 	case SIGILL:
-		return Message(
+		message=Message(
 			tr("Illegal Instruction Fault"),
-			tr(
-				"<p>The debugged application attempted to execute an illegal instruction.</p>"
-				"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>")
+			tr("<p>The debugged application attempted to execute an illegal instruction.</p>")
 			);
+		break;
 	case SIGFPE:
 		switch(siginfo_.si_code) {
 		case FPE_INTDIV:
-		return Message(
-			tr("Divide By Zero"),
-			tr(
-				"<p>The debugged application tried to divide an integer value by an integer divisor of zero or encountered integer division overflow.</p>"
-				"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>")
-			);
-		case FPE_FLTDIV:
-		return Message(
-			tr("Divide By Zero"),
-			tr(
-				"<p>The debugged application tried to divide an floating-point value by a floating-point divisor of zero.</p>"
-				"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>")
-			);
-		case FPE_FLTOVF:
-		return Message(
-			tr("Numeric Overflow"),
-			tr(
-				"<p>The debugged application encountered a numeric overflow while performing a floating-point computation.</p>"
-				"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>")
-			);
-		case FPE_FLTUND:
-		return Message(
-			tr("Numeric Underflow"),
-			tr(
-				"<p>The debugged application encountered a numeric underflow while performing a floating-point computation.</p>"
-				"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>")
-			);
-		case FPE_FLTRES:
-		return Message(
-			tr("Inexact Result"),
-			tr(
-				"<p>The debugged application encountered an inexact result of a floating-point computation it was performing.</p>"
-				"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>")
-			);
-		case FPE_FLTINV:
-		return Message(
-			tr("Invalid Operation"),
-			tr(
-				"<p>The debugged application attempted to perform an invalid floating-point operation.</p>"
-				"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>")
-			);
-		default:
-			return Message(
-				tr("Floating Point Exception"),
-				tr(
-					"<p>The debugged application encountered a floating-point exception.</p>"
-					"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>")
+			message=Message(
+				tr("Divide By Zero"),
+				tr("<p>The debugged application tried to divide an integer value by an integer divisor of zero or encountered integer division overflow.</p>")
 				);
+			break;
+		case FPE_FLTDIV:
+			message=Message(
+				tr("Divide By Zero"),
+				tr("<p>The debugged application tried to divide an floating-point value by a floating-point divisor of zero.</p>")
+				);
+			break;
+		case FPE_FLTOVF:
+			message=Message(
+				tr("Numeric Overflow"),
+				tr("<p>The debugged application encountered a numeric overflow while performing a floating-point computation.</p>")
+				);
+			break;
+		case FPE_FLTUND:
+			message=Message(
+				tr("Numeric Underflow"),
+				tr("<p>The debugged application encountered a numeric underflow while performing a floating-point computation.</p>")
+				);
+			break;
+		case FPE_FLTRES:
+			message=Message(
+				tr("Inexact Result"),
+				tr("<p>The debugged application encountered an inexact result of a floating-point computation it was performing.</p>")
+				);
+			break;
+		case FPE_FLTINV:
+			message=Message(
+				tr("Invalid Operation"),
+				tr("<p>The debugged application attempted to perform an invalid floating-point operation.</p>")
+				);
+			break;
+		default:
+			message=Message(
+				tr("Floating Point Exception"),
+				tr("<p>The debugged application encountered a floating-point exception.</p>")
+				);
+			break;
 		}
+		break;
 
 	case SIGABRT:
-		return Message(
+		message=Message(
 			tr("Application Aborted"),
-			tr(
-				"<p>The debugged application has aborted.</p>"
-				"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>")
+			tr("<p>The debugged application has aborted.</p>")
 			);
+		break;
 	case SIGBUS:
-		return Message(
+		message=Message(
 			tr("Bus Error"),
-			tr(
-				"<p>The debugged application received a bus error. Typically, this means that it tried to read or write data that is misaligned.</p>"
-				"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>")
+			tr("<p>The debugged application received a bus error. Typically, this means that it tried to read or write data that is misaligned.</p>")
 			);
+		break;
 #ifdef SIGSTKFLT
 	case SIGSTKFLT:
-		return Message(
+		message=Message(
 			tr("Stack Fault"),
-			tr(
-				"<p>The debugged application encountered a stack fault.</p>"
-				"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>")
+			tr("<p>The debugged application encountered a stack fault.</p>")
 			);
+		break;
 #endif
 	case SIGPIPE:
-		return Message(
+		message=Message(
 			tr("Broken Pipe Fault"),
-			tr(
-				"<p>The debugged application encountered a broken pipe fault.</p>"
-				"<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>")
+			tr("<p>The debugged application encountered a broken pipe fault.</p>")
 			);
+		break;
 #ifdef SIGHUP
 	case SIGHUP:
-		return createUnexpectedSignalMessage("SIGHUP", SIGHUP);
+		message=createUnexpectedSignalMessage("SIGHUP", SIGHUP);
+		break;
 #endif
 #ifdef SIGINT
 	case SIGINT:
-		return createUnexpectedSignalMessage("SIGINT", SIGINT);
+		message=createUnexpectedSignalMessage("SIGINT", SIGINT);
+		break;
 #endif
 #ifdef SIGQUIT
 	case SIGQUIT:
-		return createUnexpectedSignalMessage("SIGQUIT", SIGQUIT);
+		message=createUnexpectedSignalMessage("SIGQUIT", SIGQUIT);
+		break;
 #endif
 #ifdef SIGTRAP
 	case SIGTRAP:
-		return createUnexpectedSignalMessage("SIGTRAP", SIGTRAP);
+		message=createUnexpectedSignalMessage("SIGTRAP", SIGTRAP);
+		break;
 #endif
 #ifdef SIGKILL
 	case SIGKILL:
-		return createUnexpectedSignalMessage("SIGKILL", SIGKILL);
+		message=createUnexpectedSignalMessage("SIGKILL", SIGKILL);
+		break;
 #endif
 #ifdef SIGUSR1
 	case SIGUSR1:
-		return createUnexpectedSignalMessage("SIGUSR1", SIGUSR1);
+		message=createUnexpectedSignalMessage("SIGUSR1", SIGUSR1);
+		break;
 #endif
 #ifdef SIGUSR2
 	case SIGUSR2:
-		return createUnexpectedSignalMessage("SIGUSR2", SIGUSR2);
+		message=createUnexpectedSignalMessage("SIGUSR2", SIGUSR2);
+		break;
 #endif
 #ifdef SIGALRM
 	case SIGALRM:
-		return createUnexpectedSignalMessage("SIGALRM", SIGALRM);
+		message=createUnexpectedSignalMessage("SIGALRM", SIGALRM);
+		break;
 #endif
 #ifdef SIGTERM
 	case SIGTERM:
-		return createUnexpectedSignalMessage("SIGTERM", SIGTERM);
+		message=createUnexpectedSignalMessage("SIGTERM", SIGTERM);
+		break;
 #endif
 #ifdef SIGCHLD
 	case SIGCHLD:
-		return createUnexpectedSignalMessage("SIGCHLD", SIGCHLD);
+		message=createUnexpectedSignalMessage("SIGCHLD", SIGCHLD);
+		break;
 #endif
 #ifdef SIGCONT
 	case SIGCONT:
-		return createUnexpectedSignalMessage("SIGCONT", SIGCONT);
+		message=createUnexpectedSignalMessage("SIGCONT", SIGCONT);
+		break;
 #endif
 #ifdef SIGSTOP
 	case SIGSTOP:
-		return createUnexpectedSignalMessage("SIGSTOP", SIGSTOP);
+		message=createUnexpectedSignalMessage("SIGSTOP", SIGSTOP);
+		break;
 #endif
 #ifdef SIGTSTP
 	case SIGTSTP:
-		return createUnexpectedSignalMessage("SIGTSTP", SIGTSTP);
+		message=createUnexpectedSignalMessage("SIGTSTP", SIGTSTP);
+		break;
 #endif
 #ifdef SIGTTIN
 	case SIGTTIN:
-		return createUnexpectedSignalMessage("SIGTTIN", SIGTTIN);
+		message=createUnexpectedSignalMessage("SIGTTIN", SIGTTIN);
+		break;
 #endif
 #ifdef SIGTTOU
 	case SIGTTOU:
-		return createUnexpectedSignalMessage("SIGTTOU", SIGTTOU);
+		message=createUnexpectedSignalMessage("SIGTTOU", SIGTTOU);
+		break;
 #endif
 #ifdef SIGURG
 	case SIGURG:
-		return createUnexpectedSignalMessage("SIGURG", SIGURG);
+		message=createUnexpectedSignalMessage("SIGURG", SIGURG);
+		break;
 #endif
 #ifdef SIGXCPU
 	case SIGXCPU:
-		return createUnexpectedSignalMessage("SIGXCPU", SIGXCPU);
+		message=createUnexpectedSignalMessage("SIGXCPU", SIGXCPU);
+		break;
 #endif
 #ifdef SIGXFSZ
 	case SIGXFSZ:
-		return createUnexpectedSignalMessage("SIGXFSZ", SIGXFSZ);
+		message=createUnexpectedSignalMessage("SIGXFSZ", SIGXFSZ);
+		break;
 #endif
 #ifdef SIGVTALRM
 	case SIGVTALRM:
-		return createUnexpectedSignalMessage("SIGVTALRM", SIGVTALRM);
+		message=createUnexpectedSignalMessage("SIGVTALRM", SIGVTALRM);
+		break;
 #endif
 #ifdef SIGPROF
 	case SIGPROF:
-		return createUnexpectedSignalMessage("SIGPROF", SIGPROF);
+		message=createUnexpectedSignalMessage("SIGPROF", SIGPROF);
+		break;
 #endif
 #ifdef SIGWINCH
 	case SIGWINCH:
-		return createUnexpectedSignalMessage("SIGWINCH", SIGWINCH);
+		message=createUnexpectedSignalMessage("SIGWINCH", SIGWINCH);
+		break;
 #endif
 #ifdef SIGIO
 	case SIGIO:
-		return createUnexpectedSignalMessage("SIGIO", SIGIO);
+		message=createUnexpectedSignalMessage("SIGIO", SIGIO);
+		break;
 #endif
 	default:
 		return Message();
 	}
+
+	message.message+="<p>If you would like to pass this exception to the application press Shift+[F7/F8/F9]</p>";
+	return message;
 }
 
 //------------------------------------------------------------------------------
