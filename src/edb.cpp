56305e48ac7465750dac0be5085780d7a1cc2e9e
Ruslan Kabatsayev <b7.10110111@gmail.com>
2016-04-28 17:12:35 +0300

    Fix strange behavior of QStatusBar::showMessage()

--- src/edb.cpp
+++ src/edb.cpp
@@ -1254,10 +1254,18 @@ address_t current_data_view_address() {
 //------------------------------------------------------------------------------
 void set_status(const QString &message, int timeoutMillisecs) {
 	ui()->ui.statusbar->showMessage(message, timeoutMillisecs);
+	// FIXME: For some reason, despite showMessage() calls repaint, there's some
+	// hysteresis in actual look of the status bar: in a busy loop of calls to set_status()
+	// it updates to previous content. In some cases it even doesn't actually update.
+	// This happens at least on Qt 4.
+	// Manual call to repaint makes it show the correct text immediately.
+	ui()->ui.statusbar->repaint();
 }
 
 void clear_status() {
 	ui()->ui.statusbar->clearMessage();
+	// FIXME: same comment applies as in set_status()
+	ui()->ui.statusbar->repaint();
 }
 
 //------------------------------------------------------------------------------
