01901d25f08d47eb755e50e99f8f70a3f94f069c
Aaron Opfer <me@aaronopfer.com>
2016-11-20 00:53:29 -0600

    Annotate constants that appear to point to ASCII

--- src/widgets/QDisassemblyView.cpp
+++ src/widgets/QDisassemblyView.cpp
@@ -870,13 +870,10 @@ void QDisassemblyView::paintEvent(QPaintEvent *) {
 			}
 		} else
 
-#if 1
 		// highlight header of binary
 		if(binary_info && address >= region_->start() && address - region_->start() < binary_info->header_size()) {
 			painter.fillRect(0, y, width(), line_height, QBrush(Qt::lightGray));
-		} else
-#endif
-		if(row_index & 1) {
+		} else if(row_index & 1) {
 			// draw alternating line backgrounds
 			painter.fillRect(0, y, width(), line_height, alternated_base_color);
 		}
@@ -986,6 +983,40 @@ void QDisassemblyView::paintEvent(QPaintEvent *) {
 				line_height,
 				Qt::AlignCenter,
 				comment);
+		} else if (inst && !is_jump(inst) && !is_call(inst)) {
+			// draw ascii representations of immediate constants
+			unsigned int op_count = inst.operand_count();
+			QString immed_string;
+			for (unsigned int op_idx = 0; op_idx < op_count; op_idx++) {
+				auto oper = inst.operands()[op_idx];
+				edb::address_t address = 0;
+				if (oper.type() == edb::Operand::TYPE_REL || oper.type() == edb::Operand::TYPE_IMMEDIATE) {
+					address = oper.relative_target();
+				} else if (oper.type() == edb::Operand::TYPE_EXPRESSION) {
+					// TODO Interact with capstone to evaluate operand's displacement
+				}
+
+				if (address > 0x10000) { // FIXME use page size
+					QString string_param;
+					int string_length;
+
+					if (edb::v1::get_ascii_string_at_address(address, string_param, edb::v1::config().min_string_length, 256, string_length)) {
+						immed_string.append(
+							QString("ASCII \"%1\" ").arg(string_param)
+						);
+					}
+				}
+			}
+			if (immed_string.length()) {
+				painter.drawText(
+					l3 + font_width_ + (font_width_ / 2),
+					y,
+					immed_string.length() * font_width_,
+					line_height,
+					Qt::AlignCenter,
+					immed_string
+				);
+			}
 		}
 
 		y += line_height;
