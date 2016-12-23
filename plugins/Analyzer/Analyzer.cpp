76fe545b9390d41d2f5365cf785fcd455a67b5a5
Evan Teran <evan.teran@gmail.com>
2016-12-23 14:58:06 -0500

    Qt4 needs these includes

--- plugins/Analyzer/Analyzer.cpp
+++ plugins/Analyzer/Analyzer.cpp
@@ -31,6 +31,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "Configuration.h"
 
 #include <QCoreApplication>
+#include <QDir>
+#include <QFileInfo>
 #include <QHash>
 #include <QMainWindow>
 #include <QMenu>
