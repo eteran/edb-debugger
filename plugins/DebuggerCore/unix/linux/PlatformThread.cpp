526ffbb4aafbe37606cd311b95dfec2e03b140b7
Evan Teran <evan.teran@gmail.com>
2017-04-24 20:04:16 -0400

    formatting

--- plugins/DebuggerCore/unix/linux/PlatformThread.cpp
+++ plugins/DebuggerCore/unix/linux/PlatformThread.cpp
@@ -309,47 +309,59 @@ void PlatformThread::get_state(State *state) {
 	core_->detectDebuggeeBitness();
 
 	if(auto state_impl = static_cast<PlatformState *>(state->impl_)) {
+	
 		// State must be cleared before filling to zero all presence flags, otherwise something
 		// may remain not updated. Also, this way we'll mark all the unfilled values.
 		state_impl->clear();
 
-			if(EDB_IS_64_BIT)
-				fillStateFromSimpleRegs(state_impl); // 64-bit GETREGS call always returns 64-bit state, so use it
-			else if(!fillStateFromPrStatus(state_impl)) // if EDB is 32 bit, use GETREGSET so that we get 64-bit state for 64-bit debuggee
-				fillStateFromSimpleRegs(state_impl); // failing that, try to just get what we can
+		if(EDB_IS_64_BIT) {
+			// 64-bit GETREGS call always returns 64-bit state, so use it
+			fillStateFromSimpleRegs(state_impl); 
+		} else if(!fillStateFromPrStatus(state_impl)) {
+			// if EDB is 32 bit, use GETREGSET so that we get 64-bit state for 64-bit debuggee
+			fillStateFromSimpleRegs(state_impl);
+			// failing that, try to just get what we can
+		}
 
-			long ptraceStatus=0;
+		// First try to get full XSTATE
+		X86XState xstate;
+		struct iovec iov = { &xstate, sizeof(xstate) };
+		
+		long status = ptrace(PTRACE_GETREGSET, tid_, NT_X86_XSTATE, &iov);
+		
+		if(status == -1 || !state_impl->fillFrom(xstate,iov.iov_len)) {
 
-			// First try to get full XSTATE
-			X86XState xstate;
-			iovec iov={&xstate,sizeof(xstate)};
-			ptraceStatus=ptrace(PTRACE_GETREGSET, tid_, NT_X86_XSTATE, &iov);
-			if(ptraceStatus==-1 || !state_impl->fillFrom(xstate,iov.iov_len)) {
-
-				// No XSTATE available, get just floating point and SSE registers
-				static bool getFPXRegsSupported=(EDB_IS_32_BIT ? true : false);
-				UserFPXRegsStructX86 fpxregs;
-				// This should be automatically optimized out on amd64. If not, not a big deal.
-				// Avoiding conditional compilation to facilitate syntax error checking
-				if(getFPXRegsSupported)
-					getFPXRegsSupported=(ptrace(PTRACE_GETFPXREGS, tid_, 0, &fpxregs)!=-1);
-
-				if(getFPXRegsSupported) {
-					state_impl->fillFrom(fpxregs);
-				} else {
-					// No GETFPXREGS: on x86 this means SSE is not supported
-					//                on x86_64 FPREGS already contain SSE state
-					user_fpregs_struct fpregs;
-					if((ptraceStatus=ptrace(PTRACE_GETFPREGS, tid_, 0, &fpregs))!=-1)
-						state_impl->fillFrom(fpregs);
-					else
-						perror("PTRACE_GETFPREGS failed");
-				}
+			// No XSTATE available, get just floating point and SSE registers
+			static bool getFPXRegsSupported = EDB_IS_32_BIT;
+			
+			UserFPXRegsStructX86 fpxregs;
+			
+			// This should be automatically optimized out on amd64. If not, not a big deal.
+			// Avoiding conditional compilation to facilitate syntax error checking
+			if(getFPXRegsSupported) {
+				getFPXRegsSupported = (ptrace(PTRACE_GETFPXREGS, tid_, 0, &fpxregs) != -1);
 			}
 
-			// debug registers
-			for(std::size_t i=0;i<8;++i)
-				state_impl->x86.dbgRegs[i] = get_debug_register(i);
+			if(getFPXRegsSupported) {
+				state_impl->fillFrom(fpxregs);
+			} else {
+				// No GETFPXREGS: on x86 this means SSE is not supported
+				//                on x86_64 FPREGS already contain SSE state
+				struct user_fpregs_struct fpregs;
+				status = ptrace(PTRACE_GETFPREGS, tid_, 0, &fpregs);
+				
+				if(status != -1) {
+					state_impl->fillFrom(fpregs);
+				} else {
+					perror("PTRACE_GETFPREGS failed");
+				}
+			}
+		}
+
+		// debug registers
+		for(std::size_t i = 0; i < 8; ++i) {
+			state_impl->x86.dbgRegs[i] = get_debug_register(i);
+		}
 	}
 }
 
@@ -362,53 +374,64 @@ void PlatformThread::set_state(const State &state) {
 	// TODO: assert that we are paused
 
 	if(auto state_impl = static_cast<PlatformState *>(state.impl_)) {
-		bool setPrStatusDone=false;
+		bool setPrStatusDone = false;
+
 		if(EDB_IS_32_BIT && state_impl->is64Bit()) {
 			// Try to set 64-bit state
 			PrStatus_X86_64 prstat64;
 			state_impl->fillStruct(prstat64);
-			iovec prstat_iov = {&prstat64, sizeof(prstat64)};
-			if(ptrace(PTRACE_SETREGSET, tid_, NT_PRSTATUS, &prstat_iov) != -1)
-				setPrStatusDone=true;
-			else
+	
+			struct iovec prstat_iov = { &prstat64, sizeof(prstat64) };
+			if(ptrace(PTRACE_SETREGSET, tid_, NT_PRSTATUS, &prstat_iov) != -1) {
+				setPrStatusDone = true;
+			} else {
 				perror("PTRACE_SETREGSET failed");
+			}
 		}
+		
 		// Fallback to setting 32-bit set
 		if(!setPrStatusDone) {
-			user_regs_struct regs;
+			struct user_regs_struct regs;
 			state_impl->fillStruct(regs);
 			ptrace(PTRACE_SETREGS, tid_, 0, &regs);
 		}
 
 		// debug registers
-		for(std::size_t i=0;i<8;++i)
-			set_debug_register(i,state_impl->x86.dbgRegs[i]);
+		for(std::size_t i = 0;i < 8; ++i) {
+			set_debug_register(i, state_impl->x86.dbgRegs[i]);
+		}
 
-		static bool xsaveSupported=true; // hope for the best, adjust for reality
+		// hope for the best, adjust for reality
+		static bool xsaveSupported = true;
+		
 		if(xsaveSupported) {
 			X86XState xstate;
-			const auto size=state_impl->fillStruct(xstate);
-			iovec iov={&xstate,size};
-			if(ptrace(PTRACE_SETREGSET, tid_, NT_X86_XSTATE, &iov)==-1)
-				xsaveSupported=false;
+			const auto size = state_impl->fillStruct(xstate);
+			struct iovec iov = { &xstate, size };
+			if(ptrace(PTRACE_SETREGSET, tid_, NT_X86_XSTATE, &iov) == -1) {
+				xsaveSupported = false;
+			}
 		}
+		
 		// If xsave/xrstor appears unsupported, fallback to fxrstor
 		// NOTE: it's not "else", it's an independent check for possibly modified flag
 		if(!xsaveSupported) {
-			static bool setFPXRegsSupported=EDB_IS_32_BIT;
+			static bool setFPXRegsSupported = EDB_IS_32_BIT;
 			if(setFPXRegsSupported) {
 				UserFPXRegsStructX86 fpxregs;
 				state_impl->fillStruct(fpxregs);
-				setFPXRegsSupported=(ptrace(PTRACE_SETFPXREGS, tid_, 0, &fpxregs)!=-1);
+				setFPXRegsSupported = (ptrace(PTRACE_SETFPXREGS, tid_, 0, &fpxregs) != -1);
 			}
+			
 			if(!setFPXRegsSupported) {
 				// No SETFPXREGS: on x86 this means SSE is not supported
 				//                on x86_64 FPREGS already contain SSE state
 				// Just set fpregs then
-				user_fpregs_struct fpregs;
+				struct user_fpregs_struct fpregs;
 				state_impl->fillStruct(fpregs);
-				if(ptrace(PTRACE_SETFPREGS, tid_, 0, &fpregs)==-1)
+				if(ptrace(PTRACE_SETFPREGS, tid_, 0, &fpregs) == -1) {
 					perror("PTRACE_SETFPREGS failed");
+				}
 			}
 		}
 	}
