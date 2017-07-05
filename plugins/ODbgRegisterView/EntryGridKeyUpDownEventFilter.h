836f1aac9e7369c13a624e0b9d80982defe12bf0
Ruslan Kabatsayev <b7.10110111@gmail.com>
2017-07-05 16:52:14 +0300

    Add a function which, when called from event filter of a lineEdit-grid-based register editor, will make up/down arrow keys switch focus to the lineEdit above/below the current one

---
+++ plugins/ODbgRegisterView/EntryGridKeyUpDownEventFilter.h
@@ -0,0 +1,12 @@
+#ifndef ENTRY_GRID_KEY_UP_DOWN_EVENT_FILTER_H_20170705
+#define ENTRY_GRID_KEY_UP_DOWN_EVENT_FILTER_H_20170705
+
+class QWidget;
+class QObject;
+class QEvent;
+
+namespace ODbgRegisterView {
+bool entryGridKeyUpDownEventFilter(QWidget* parent, QObject* obj, QEvent* event);
+}
+
+#endif
