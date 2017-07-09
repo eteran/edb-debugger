3d8c278b65a2cbdf9934dd734e5cfbf38f8c786c
Evan Teran <evan.teran@gmail.com>
2017-07-09 02:06:48 -0400

    isolating some structures so it's cleaner to add more arches I *think* unsigned long on ARM 32-bit is a uint32_t. But I'd like to double check that.

--- plugins/DebuggerCore/unix/linux/PlatformState.h
+++ plugins/DebuggerCore/unix/linux/PlatformState.h
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "IState.h"
 #include "Types.h"
+#include "PrStatus.h"
 #include "edb.h"
 #include <cstddef>
 #include <sys/user.h>
@@ -158,59 +159,7 @@ struct UserFPRegsStructX86 {
 };
 #endif
 
-struct PrStatus_X86 {
-	uint32_t ebx;
-	uint32_t ecx;
-	uint32_t edx;
-	uint32_t esi;
-	uint32_t edi;
-	uint32_t ebp;
-	uint32_t eax;
-	uint32_t ds;
-	uint32_t es;
-	uint32_t fs;
-	uint32_t gs;
-	uint32_t orig_eax;
-	uint32_t eip;
-	uint32_t cs;
-	uint32_t eflags;
-	uint32_t esp;
-	uint32_t ss;
-};
 
-static_assert(sizeof(PrStatus_X86) == 68, "PrStatus_X86 is messed up!");
-
-struct PrStatus_X86_64 {
-	uint64_t r15;
-	uint64_t r14;
-	uint64_t r13;
-	uint64_t r12;
-	uint64_t rbp;
-	uint64_t rbx;
-	uint64_t r11;
-	uint64_t r10;
-	uint64_t r9;
-	uint64_t r8;
-	uint64_t rax;
-	uint64_t rcx;
-	uint64_t rdx;
-	uint64_t rsi;
-	uint64_t rdi;
-	uint64_t orig_rax;
-	uint64_t rip;
-	uint64_t cs;
-	uint64_t rflags;
-	uint64_t rsp;
-	uint64_t ss;
-	uint64_t fs_base;
-	uint64_t gs_base;
-	uint64_t ds;
-	uint64_t es;
-	uint64_t fs;
-	uint64_t gs;
-};
-
-static_assert(sizeof(PrStatus_X86_64) == 216, "PrStatus_X86_64 is messed up!");
 
 // Masks for XCR0 feature enabled bits
 #define X86_XSTATE_X87_MASK X87_XSTATE_X87
---
+++ plugins/DebuggerCore/unix/linux/PrStatus.h
@@ -0,0 +1,67 @@
+
+#ifndef PR_STATUS_H_
+#define PR_STATUS_H_
+
+#include <cstdint>
+
+struct PrStatus_X86 {
+	uint32_t ebx;
+	uint32_t ecx;
+	uint32_t edx;
+	uint32_t esi;
+	uint32_t edi;
+	uint32_t ebp;
+	uint32_t eax;
+	uint32_t ds;
+	uint32_t es;
+	uint32_t fs;
+	uint32_t gs;
+	uint32_t orig_eax;
+	uint32_t eip;
+	uint32_t cs;
+	uint32_t eflags;
+	uint32_t esp;
+	uint32_t ss;
+};
+
+static_assert(sizeof(PrStatus_X86) == 68, "PrStatus_X86 is messed up!");
+
+struct PrStatus_X86_64 {
+	uint64_t r15;
+	uint64_t r14;
+	uint64_t r13;
+	uint64_t r12;
+	uint64_t rbp;
+	uint64_t rbx;
+	uint64_t r11;
+	uint64_t r10;
+	uint64_t r9;
+	uint64_t r8;
+	uint64_t rax;
+	uint64_t rcx;
+	uint64_t rdx;
+	uint64_t rsi;
+	uint64_t rdi;
+	uint64_t orig_rax;
+	uint64_t rip;
+	uint64_t cs;
+	uint64_t rflags;
+	uint64_t rsp;
+	uint64_t ss;
+	uint64_t fs_base;
+	uint64_t gs_base;
+	uint64_t ds;
+	uint64_t es;
+	uint64_t fs;
+	uint64_t gs;
+};
+
+static_assert(sizeof(PrStatus_X86_64) == 216, "PrStatus_X86_64 is messed up!");
+
+struct PrStatus_ARM {
+	uint32_t regs[18];
+};
+
+static_assert(sizeof(PrStatus_ARM) == 72, "PrStatus_ARM is messed up!");
+
+#endif
