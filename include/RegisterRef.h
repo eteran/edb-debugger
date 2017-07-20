5e741acf50f2b15a86189540043f0285a9080235
Evan Teran <evan.teran@gmail.com>
2017-07-20 15:49:03 -0400

    adding (but not used yet) a concept of a "RegisterRef".
    This will make it MUCH easier to handle registers of various widths in a non-arch specific way.
    
    The idea is simple, the State objects will return a RegisterRef, which is basically
    an offset, size, mask tuple. The offset is will basically be something like:
    
    offsetof(State, eax) // for the eax regiser of course
    
    Then when arch specific code like in the ArchProcessor or RegiserView wants concrete values
    it can combine this with an actual State object to extract the actual value. This way, we
    don't need to have hacks to deal with things like "ARM doesn't have an 80-bit float type".
    
    This is based on Mozilla's approach, which seems highly effective.

---
+++ include/RegisterRef.h
@@ -0,0 +1,67 @@
+
+// based on code from mozilla: https://github.com/mozilla/rr/blob/master/src/Registers.cc
+
+#ifndef REGISTER_REF_H_
+#define REGISTER_REF_H_
+
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <climits>
+
+namespace DebuggerCorePlugin {
+
+class RegisterRef {
+public:
+	constexpr RegisterRef() : name(nullptr), offset(0), nbytes(0), comparison_mask(0) {
+	}
+
+	RegisterRef(const char *name_, std::size_t offset_, std::size_t nbytes_) : name(name_), offset(offset_), nbytes(nbytes_) {
+		comparison_mask = mask_for_nbytes(nbytes_);
+	}
+
+	RegisterRef(const char *name_, std::size_t offset_, std::size_t nbytes_, std::uint64_t comparison_mask_) : name(name_), offset(offset_), nbytes(nbytes_), comparison_mask(comparison_mask_) {
+		// Ensure no bits are set outside of the register's bitwidth.
+		assert((comparison_mask_ & ~mask_for_nbytes(nbytes_)) == 0);
+	}
+	
+	RegisterRef(const RegisterRef &) = default;
+	
+	RegisterRef &operator=(const RegisterRef &) = default;
+
+	// Returns a pointer to the register in |regs| represented by |offset|.
+	// |regs| is assumed to be a pointer to the user_struct_regs for the
+	// appropriate architecture.
+	void *pointer_into(void *regs) const {
+		return static_cast<char *>(regs) + offset;
+	}
+
+	const void *pointer_into(const void *regs) const {
+		return static_cast<const char *>(regs) + offset;
+	}
+
+public:
+	static std::uint64_t mask_for_nbytes(std::size_t nbytes) {
+		assert(nbytes <= sizeof(comparison_mask));
+		return ((nbytes == sizeof(comparison_mask)) ? std::uint64_t(0) : (std::uint64_t(1) << nbytes * CHAR_BIT)) - 1;
+	}
+
+public:
+	// The name of this register.
+	const char *name;
+
+	// The offsetof the register in state structure.
+	std::size_t offset;
+
+	// The size of the register.  0 means we cannot read it.
+	std::size_t nbytes;
+
+	// Mask to be applied to register values prior to comparing them.  Will
+	// typically be ((1 << nbytes) - 1), but some registers may have special
+	// comparison semantics.
+	std::uint64_t comparison_mask;
+};
+
+}
+
+#endif
