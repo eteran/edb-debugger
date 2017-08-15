71c1f09eb4acb0a23577150f13eb4a4822b008bd
Ruslan Kabatsayev <b7.10110111@gmail.com>
2017-08-15 19:53:25 +0300

    Add a simple arch-agnostic register view
    It allows to at least see what is present in the model when we port EDB
    to a new architecture where the arch-centric register view hasn't yet
    been ported.
    It's disabled by default, and can be enabled by passing
    -DBUILD_SIMPLE_REGISTER_VIEW=1 to cmake.

---
+++ plugins/SimpleRegView/Plugin.h
@@ -0,0 +1,48 @@
+/*
+Copyright (C) 2017 - 2017 Ruslan Kabatsayev
+                          b7.10110111@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SIMPLE_REG_VIEW_PLUGIN_H_20170815
+#define SIMPLE_REG_VIEW_PLUGIN_H_20170815
+
+#include "IPlugin.h"
+#include "Types.h"
+
+namespace SimpleRegView
+{
+
+class RegView;
+
+class Plugin : public QObject, public IPlugin {
+	Q_OBJECT
+	Q_INTERFACES(IPlugin)
+#if QT_VERSION >= 0x050000
+	Q_PLUGIN_METADATA(IID "edb.IPlugin/1.0")
+#endif
+	Q_CLASSINFO("author", "Ruslan Kabatsayev")
+	Q_CLASSINFO("email", "b7.10110111@gmail.com")
+
+public:
+	Plugin();
+	QMenu* menu(QWidget*);
+private:
+	RegView* regView;
+};
+
+}
+
+#endif
