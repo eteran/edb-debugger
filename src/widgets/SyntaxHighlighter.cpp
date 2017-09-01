34d2655a77cd6aa59c54d2111f230d3dee1e23ab
Ruslan Kabatsayev <b7.10110111@gmail.com>
2017-09-01 15:01:32 +0300

    Optimize syntax highlighter by changing regex groups to non-capturing groups

--- src/widgets/SyntaxHighlighter.cpp
+++ src/widgets/SyntaxHighlighter.cpp
@@ -76,7 +76,7 @@ void SyntaxHighlighter::create_rules() {
 
 	// comma
 	rules_.push_back(HighlightingRule(
-		"(,)",
+		"(?:,)",
 		QColor(settings.value("theme.brackets.foreground", "blue").toString()),
 		QColor(settings.value("theme.brackets.background", "transparent").toString()),
 		settings.value("theme.brackets.weight", QFont::Normal).toInt(),
@@ -86,7 +86,7 @@ void SyntaxHighlighter::create_rules() {
 
 	// expression brackets
 	rules_.push_back(HighlightingRule(
-		"([\\(\\)\\[\\]])",
+		"(?:[\\(?:\\)\\[\\]])",
 		QColor(settings.value("theme.brackets.foreground", "blue").toString()),
 		QColor(settings.value("theme.brackets.background", "transparent").toString()),
 		settings.value("theme.brackets.weight", QFont::Normal).toInt(),
@@ -96,7 +96,7 @@ void SyntaxHighlighter::create_rules() {
 
 	// math operators
 	rules_.push_back(HighlightingRule(
-		"\\b([\\+\\-\\*])\\b",
+		"\\b(?:[\\+\\-\\*])\\b",
 		QColor(settings.value("theme.operator.foreground", "blue").toString()),
 		QColor(settings.value("theme.operator.background", "transparent").toString()),
 		settings.value("theme.operator.weight", QFont::Normal).toInt(),
@@ -108,11 +108,11 @@ void SyntaxHighlighter::create_rules() {
 	// TODO: support ST(N)
 	rules_.push_back(HighlightingRule(
 #if defined EDB_X86 || defined EDB_X86_64
-		"\\b(((e|r)?(ax|bx|cx|dx|bp|sp|si|di|ip))|([abcd](l|h))|(sp|bp|si|di)l|([cdefgs]s)|[xyz]?mm([0-9]|[12][0-9]|3[01])|r(8|9|(1[0-5]))[dwb]?)\\b",
+		"\\b(?:(?:(?:e|r)?(?:ax|bx|cx|dx|bp|sp|si|di|ip))|(?:[abcd](?:l|h))|(?:sp|bp|si|di)l|(?:[cdefgs]s)|[xyz]?mm(?:[0-9]|[12][0-9]|3[01])|r(?:8|9|(?:1[0-5]))[dwb]?)\\b",
 #elif defined EDB_ARM32
-		"\\b(r([0-9]|1[0-5])|sb|sl|fp|ip|sp|lr|pc|[sd][0-9]|[sdf]([12][0-9]|3[01])|q([0-9]|1[0-5]))\\b",
+		"\\b(?:r(?:[0-9]|1[0-5])|sb|sl|fp|ip|sp|lr|pc|[sd][0-9]|[sdf](?:[12][0-9]|3[01])|q(?:[0-9]|1[0-5]))\\b",
 #elif defined EDB_ARM64
-		"\\b([xw]([12]?[0-9]|3[01]))\\b"/* FIXME: stub, only GPRs here */,
+		"\\b(?:[xw](?:[12]?[0-9]|3[01]))\\b"/* FIXME: stub, only GPRs here */,
 #else
 #error "What string should be here?"
 #endif
@@ -128,7 +128,7 @@ void SyntaxHighlighter::create_rules() {
 #if defined EDB_ARM32 || defined EDB_ARM64
 		"#?" /* concatenated with general number pattern */
 #endif
-		"\\b((0[0-7]*)|(0(x|X)[0-9a-fA-F]+)|([1-9][0-9]*))\\b",
+		"\\b(?:(?:0[0-7]*)|(?:0(?:x|X)[0-9a-fA-F]+)|(?:[1-9][0-9]*))\\b",
 		QColor(settings.value("theme.constant.foreground", "black").toString()),
 		QColor(settings.value("theme.constant.background", "transparent").toString()),
 		settings.value("theme.constant.weight", QFont::Normal).toInt(),
@@ -139,7 +139,7 @@ void SyntaxHighlighter::create_rules() {
 #if defined EDB_X86 || defined EDB_X86_64
 	// pointer modifiers
 	rules_.push_back(HighlightingRule(
-		"\\b(t?byte|([xyz]mm|[qdf]?)word)( ptr)?\\b",
+		"\\b(?:t?byte|(?:[xyz]mm|[qdf]?)word)(?: ptr)?\\b",
 		QColor(settings.value("theme.ptr.foreground", "darkGreen").toString()),
 		QColor(settings.value("theme.ptr.background", "transparent").toString()),
 		settings.value("theme.ptr.weight", QFont::Normal).toInt(),
@@ -149,7 +149,7 @@ void SyntaxHighlighter::create_rules() {
 
 	// prefix
 	rules_.push_back(HighlightingRule(
-		"\\b(lock|rep(ne)?)\\b",
+		"\\b(?:lock|rep(?:ne)?)\\b",
 		QColor(settings.value("theme.prefix.foreground", "black").toString()),
 		QColor(settings.value("theme.prefix.background", "transparent").toString()),
 		settings.value("theme.prefix.weight", QFont::Bold).toInt(),
@@ -162,11 +162,11 @@ void SyntaxHighlighter::create_rules() {
 	// flow control
 	rules_.push_back(HighlightingRule(
 #if defined EDB_X86 || defined EDB_X86_64
-		"\\b(l?jmp[bswlqt]?|loopn?[ez]|(jn?(a|ae|b|be|c|e|g|ge|l|le|o|p|s|z)|j(pe|po|cxz|ecxz)))\\b",
+		"\\b(?:l?jmp[bswlqt]?|loopn?[ez]|(?:jn?(?:a|ae|b|be|c|e|g|ge|l|le|o|p|s|z)|j(?:pe|po|cxz|ecxz)))\\b",
 #elif defined EDB_ARM32 || defined EDB_ARM64
 		/* FIXME(ARM): there are also instructions like `add pc, pc, #5`, which
 		 *             should also be considered flow control */
-		"\\b(b(x|xj)?(eq|ne|cs|hs|cc|lo|mi|pl|vs|vc|hi|ls|ge|lt|gt|le)?)\\b",
+		"\\b(?:b(?:x|xj)?(?:eq|ne|cs|hs|cc|lo|mi|pl|vs|vc|hi|ls|ge|lt|gt|le)?)\\b",
 #else
 #error "What string should be here?"
 #endif
@@ -181,9 +181,9 @@ void SyntaxHighlighter::create_rules() {
 	// function call
 	rules_.push_back(HighlightingRule(
 #if defined EDB_X86 || defined EDB_X86_64
-		"\\b(call|ret[nf]?)[bswlqt]?\\b",
+		"\\b(?:call|ret[nf]?)[bswlqt]?\\b",
 #elif defined EDB_ARM32 || defined EDB_ARM64
-		"\\b(b(l|lx)(eq|ne|cs|hs|cc|lo|mi|pl|vs|vc|hi|ls|ge|lt|gt|le)?)\\b",
+		"\\b(?:b(?:l|lx)(?:eq|ne|cs|hs|cc|lo|mi|pl|vs|vc|hi|ls|ge|lt|gt|le)?)\\b",
 #else
 #error "What string should be here?"
 #endif
@@ -199,7 +199,7 @@ void SyntaxHighlighter::create_rules() {
 
 	// stack operations
 	rules_.push_back(HighlightingRule(
-		"\\b(pushf?|popf?|enter|leave)\\b",
+		"\\b(?:pushf?|popf?|enter|leave)\\b",
 		QColor(settings.value("theme.stack.foreground", "blue").toString()),
 		QColor(settings.value("theme.stack.background", "transparent").toString()),
 		settings.value("theme.stack.weight", QFont::Normal).toInt(),
@@ -209,7 +209,7 @@ void SyntaxHighlighter::create_rules() {
 
 	// comparison
 	rules_.push_back(HighlightingRule(
-		"\\b(cmp|test)[bswlqt]?\\b",
+		"\\b(?:cmp|test)[bswlqt]?\\b",
 		QColor(settings.value("theme.comparison.foreground", "blue").toString()),
 		QColor(settings.value("theme.comparison.background", "transparent").toString()),
 		settings.value("theme.comparison.weight", QFont::Normal).toInt(),
@@ -220,7 +220,7 @@ void SyntaxHighlighter::create_rules() {
 
 	// data transfer
 	rules_.push_back(HighlightingRule(
-		"\\b(c?movs[bw]|lea|xchg|mov([zs]x?)?)[bswlqt]?\\b",
+		"\\b(?:c?movs[bw]|lea|xchg|mov(?:[zs]x?)?)[bswlqt]?\\b",
 		QColor(settings.value("theme.data_xfer.foreground", "blue").toString()),
 		QColor(settings.value("theme.data_xfer.background", "transparent").toString()),
 		settings.value("theme.data_xfer.weight", QFont::Normal).toInt(),
@@ -230,7 +230,7 @@ void SyntaxHighlighter::create_rules() {
 
 	// arithmetic
 	rules_.push_back(HighlightingRule(
-		"\\b(add|sub|i?mul|i?div|neg|adc|sbb|inc|dec)[bswlqt]?\\b",
+		"\\b(?:add|sub|i?mul|i?div|neg|adc|sbb|inc|dec)[bswlqt]?\\b",
 		QColor(settings.value("theme.arithmetic.foreground", "blue").toString()),
 		QColor(settings.value("theme.arithmetic.background", "transparent").toString()),
 		settings.value("theme.arithmetic.weight", QFont::Normal).toInt(),
@@ -240,7 +240,7 @@ void SyntaxHighlighter::create_rules() {
 
 	// logic
 	rules_.push_back(HighlightingRule(
-		"\\b(and|x?or|not)[bswlqt]?\\b",
+		"\\b(?:and|x?or|not)[bswlqt]?\\b",
 		QColor(settings.value("theme.logic.foreground", "blue").toString()),
 		QColor(settings.value("theme.logic.background", "transparent").toString()),
 		settings.value("theme.logic.weight", QFont::Normal).toInt(),
@@ -250,7 +250,7 @@ void SyntaxHighlighter::create_rules() {
 
 	// shift
 	rules_.push_back(HighlightingRule(
-		"\\b(sh|sa|sc|ro)[rl][bswlqt]?\\b",
+		"\\b(?:sh|sa|sc|ro)[rl][bswlqt]?\\b",
 		QColor(settings.value("theme.shift.foreground", "blue").toString()),
 		QColor(settings.value("theme.shift.background", "transparent").toString()),
 		settings.value("theme.shift.weight", QFont::Normal).toInt(),
@@ -260,7 +260,7 @@ void SyntaxHighlighter::create_rules() {
 
 	// system
 	rules_.push_back(HighlightingRule(
-		"\\b(sti|cli|hlt|in|out|sysenter|sysexit|syscall|sysret|int)\\b",
+		"\\b(?:sti|cli|hlt|in|out|sysenter|sysexit|syscall|sysret|int)\\b",
 		QColor(settings.value("theme.system.foreground", "blue").toString()),
 		QColor(settings.value("theme.system.background", "transparent").toString()),
 		settings.value("theme.system.weight", QFont::Bold).toInt(),
@@ -271,7 +271,7 @@ void SyntaxHighlighter::create_rules() {
 
 	// data bytes
 	rules_.push_back(HighlightingRule(
-		"\\b(db|dw|dd|dq)\\b",
+		"\\b(?:db|dw|dd|dq)\\b",
 		QColor(settings.value("theme.data.foreground", "black").toString()),
 		QColor(settings.value("theme.data.background", "transparent").toString()),
 		settings.value("theme.data.weight", QFont::Normal).toInt(),
