d20e39a1215b256d7457c7bfa2603f7218a59a3d
Ruslan Kabatsayev <b7.10110111@gmail.com>
2017-09-07 23:34:06 +0300

    Move calculation of effective address from PlatformThread::doStep to ArchProcessor; fix treating of PC operand of BX/BLX instructions

--- src/arch/arm-generic/ArchProcessor.cpp
+++ src/arch/arm-generic/ArchProcessor.cpp
@@ -31,6 +31,75 @@ namespace {
 static constexpr size_t GPR_COUNT=16;
 }
 
+int capstoneRegToGPRIndex(int capstoneReg, bool& ok) {
+
+	ok=false;
+	int regIndex=-1;
+	// NOTE: capstone registers are stupidly not in continuous order
+	switch(capstoneReg)
+	{
+	case ARM_REG_R0:  regIndex=0; break;
+	case ARM_REG_R1:  regIndex=1; break;
+	case ARM_REG_R2:  regIndex=2; break;
+	case ARM_REG_R3:  regIndex=3; break;
+	case ARM_REG_R4:  regIndex=4; break;
+	case ARM_REG_R5:  regIndex=5; break;
+	case ARM_REG_R6:  regIndex=6; break;
+	case ARM_REG_R7:  regIndex=7; break;
+	case ARM_REG_R8:  regIndex=8; break;
+	case ARM_REG_R9:  regIndex=9; break;
+	case ARM_REG_R10: regIndex=10; break;
+	case ARM_REG_R11: regIndex=11; break;
+	case ARM_REG_R12: regIndex=12; break;
+	case ARM_REG_R13: regIndex=13; break;
+	case ARM_REG_R14: regIndex=14; break;
+	case ARM_REG_R15: regIndex=15; break;
+	default: return -1;
+	}
+	ok=true;
+	return regIndex;
+}
+
+Result<edb::address_t> ArchProcessor::get_effective_address(const edb::Instruction &insn, const edb::Operand &operand, const State &state) const {
+
+	using ResultT=Result<edb::address_t>;
+	if(!operand || !insn) return ResultT(QObject::tr("operand is invalid"),0);
+
+	const auto op=insn.operation();
+	switch(op)
+	{
+	case ARM_INS_BX:
+	case ARM_INS_BLX:
+	case ARM_INS_B:
+	case ARM_INS_BL:
+		if(is_register(operand))
+		{
+			bool ok;
+			const auto regIndex=capstoneRegToGPRIndex(operand->reg, ok);
+			if(!ok) return ResultT(QObject::tr("bad operand register for instruction %1: %2.").arg(insn.mnemonic().c_str()).arg(operand->reg), 0);
+			const auto reg=state.gp_register(regIndex);
+			if(!reg)
+				return ResultT(QObject::tr("failed to get register r%1.").arg(regIndex), 0);
+			auto value=reg.valueAsAddress();
+			if(regIndex==15) // PC
+				return ResultT(QObject::tr("unpredictable instruction"), 0);
+			return ResultT(value);
+		}
+		break;
+	}
+
+	return ResultT(QObject::tr("getting effective address for operand %1 of instruction %2 is not implemented").arg(operand.index()+1).arg(insn.mnemonic().c_str()), 0);
+}
+
+edb::address_t ArchProcessor::get_effective_address(const edb::Instruction &inst, const edb::Operand &op, const State &state, bool& ok) const {
+
+	ok=false;
+	const auto result = get_effective_address(inst, op, state);
+	if(!result) return 0;
+	return result.value();
+}
+
+
 RegisterViewModel& getModel() {
 	return static_cast<RegisterViewModel&>(edb::v1::arch_processor().get_register_view_model());
 }
