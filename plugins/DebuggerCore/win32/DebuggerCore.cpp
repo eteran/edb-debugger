141987f0064097b3dbe4449dac8a8cfb7a65ce3a
mistersandman <mr_sandman@outlook.com>
2017-09-14 17:38:55 +0200

    fix missing include

--- plugins/DebuggerCore/win32/DebuggerCore.cpp
+++ plugins/DebuggerCore/win32/DebuggerCore.cpp
@@ -26,6 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "State.h"
 #include "string_hash.h"
 
+#include <QDateTime>
 #include <QDebug>
 #include <QStringList>
 #include <QFileInfo>
