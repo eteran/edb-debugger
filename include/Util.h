6c12f430b9ba610cf66dec63c5345139d00108d4
Evan Teran <evan.teran@gmail.com>
2018-08-10 00:24:18 -0400

    squashing very pedantic warnings about passing different types to "percentage" function

--- include/Util.h
+++ include/Util.h
@@ -60,16 +60,17 @@ typename std::make_unsigned<T>::type to_unsigned(T x) { return x; }
 // Name: percentage
 // Desc: calculates how much of a multi-region byte search we have completed
 //------------------------------------------------------------------------------
-inline int percentage(int regions_finished, int regions_total, int bytes_done, int bytes_total) {
+template <class N1, class N2, class N3, class N4>
+int percentage(N1 regions_finished, N2 regions_total, N3 bytes_done, N4 bytes_total) {
 
 	// how much percent does each region account for?
-	const auto region_step      = 1.0 / static_cast<double>(regions_total) * 100;
+	const auto region_step = 1.0 / static_cast<double>(regions_total) * 100;
 
 	// how many regions are done?
 	const double regions_complete = region_step * regions_finished;
 
 	// how much of the current region is done?
-	const double region_percent   = region_step * static_cast<float>(bytes_done) / static_cast<float>(bytes_total);
+	const double region_percent = region_step * static_cast<float>(bytes_done) / static_cast<float>(bytes_total);
 
 	return static_cast<int>(regions_complete + region_percent);
 }
@@ -78,7 +79,8 @@ inline int percentage(int regions_finished, int regions_total, int bytes_done, i
 // Name: percentage
 // Desc: calculates how much of a single-region byte search we have completed
 //------------------------------------------------------------------------------
-inline int percentage(int bytes_done, int bytes_total) {
+template <class N1, class N2>
+int percentage(N1 bytes_done, N2 bytes_total) {
 	return percentage(0, 1, bytes_done, bytes_total);
 }
 
