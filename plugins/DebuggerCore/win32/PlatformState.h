4c639485a3de7fed8805ee47273136f16bcf9c57
Evan Teran <evan.teran@gmail.com>
2018-08-10 11:10:58 -0400

    same story as last win32 change, removing override to just get a build to pass while the code is out of sync with the interface

--- plugins/DebuggerCore/win32/PlatformState.h
+++ plugins/DebuggerCore/win32/PlatformState.h
@@ -32,69 +32,69 @@ public:
 	PlatformState();
 
 public:
-	IState *clone() const override;
+	IState *clone() const ;
 
 public:
-	QString flags_to_string() const override;
-	QString flags_to_string(edb::reg_t flags) const override;
-	Register value(const QString &reg) const override;
-	Register instruction_pointer_register() const override {
+	QString flags_to_string() const ;
+	QString flags_to_string(edb::reg_t flags) const ;
+	Register value(const QString &reg) const ;
+	Register instruction_pointer_register() const  {
 		qDebug("TODO: implement PlatformState::instruction_pointer_register"); return Register();
 	}
-	Register flags_register() const override {
+	Register flags_register() const  {
 		qDebug("TODO: implement PlatformState::flags_register"); return Register();
 	}
-	edb::address_t frame_pointer() const override;
-	edb::address_t instruction_pointer() const override;
-	edb::address_t stack_pointer() const override;
-	edb::reg_t debug_register(size_t n) const override;
-	edb::reg_t flags() const override;
-	long double fpu_register(int n) const override;
-	quint64 mmx_register(int n) const override;
-	QByteArray xmm_register(int n) const override;
-	void adjust_stack(int bytes) override;
-	void clear() override;
-	bool empty() const override {
+	edb::address_t frame_pointer() const ;
+	edb::address_t instruction_pointer() const ;
+	edb::address_t stack_pointer() const ;
+	edb::reg_t debug_register(size_t n) const ;
+	edb::reg_t flags() const ;
+	long double fpu_register(int n) const ;
+	quint64 mmx_register(int n) const ;
+	QByteArray xmm_register(int n) const ;
+	void adjust_stack(int bytes) ;
+	void clear() ;
+	bool empty() const  {
 		qDebug("TODO: implement PlatformState::empty"); return true;
 	}
-	void set_debug_register(size_t n, edb::reg_t value) override;
-	void set_flags(edb::reg_t flags) override;
-	void set_instruction_pointer(edb::address_t value) override;
-	void set_register(const QString &name, edb::reg_t value) override;
-	void set_register(const Register &reg) override {
+	void set_debug_register(size_t n, edb::reg_t value) ;
+	void set_flags(edb::reg_t flags) ;
+	void set_instruction_pointer(edb::address_t value) ;
+	void set_register(const QString &name, edb::reg_t value) ;
+	void set_register(const Register &reg)  {
 		qDebug("TODO: implement PlatformState::set_register");
 	}
-	Register mmx_register(size_t n) const override {
+	Register mmx_register(size_t n) const  {
 		qDebug("TODO: implement PlatformState::mmx_register"); return Register();
 	}
-	Register xmm_register(size_t n) const override {
+	Register xmm_register(size_t n) const  {
 		qDebug("TODO: implement PlatformState::xmm_register"); return Register();
 	}
-	Register ymm_register(size_t n) const override {
+	Register ymm_register(size_t n) const  {
 		qDebug("TODO: implement PlatformState::ymm_register"); return Register();
 	}
-	Register gp_register(size_t n) const override {
+	Register gp_register(size_t n) const  {
 		qDebug("TODO: implement PlatformState::gp_register"); return Register();
 	}
-	int fpu_stack_pointer() const override {
+	int fpu_stack_pointer() const  {
 		qDebug("TODO: implement PlatformState::fpu_stack_pointer"); return 0;
 	}
-	edb::value80 fpu_register(size_t n) const override {
+	edb::value80 fpu_register(size_t n) const  {
 		qDebug("TODO: implement PlatformState::fpu_register"); return edb::value80();
 	}
-	bool fpu_register_is_empty(size_t n) const override {
+	bool fpu_register_is_empty(size_t n) const  {
 		qDebug("TODO: implement PlatformState::fpu_register_is_empty"); return true;
 	}
-	QString fpu_register_tag_string(size_t n) const override {
+	QString fpu_register_tag_string(size_t n) const  {
 		qDebug("TODO: implement PlatformState::fpu_register_tag_string"); return "";
 	}
-	edb::value16 fpu_control_word() const override {
+	edb::value16 fpu_control_word() const  {
 		qDebug("TODO: implement PlatformState::fpu_control_word"); return edb::value16();
 	}
-	edb::value16 fpu_status_word() const override {
+	edb::value16 fpu_status_word() const  {
 		qDebug("TODO: implement PlatformState::fpu_status_word"); return edb::value16();
 	}
-	edb::value16 fpu_tag_word() const override {
+	edb::value16 fpu_tag_word() const  {
 		qDebug("TODO: implement PlatformState::fpu_tag_word"); return edb::value16();
 	}
 
