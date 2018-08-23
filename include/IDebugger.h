de8522ac00d20de44ff3f9459dc4c93d2b08285e
Evan Teran <evan.teran@gmail.com>
2018-08-23 14:55:59 -0400

    this include is needed for this header, was incidentally included previously

--- include/IDebugger.h
+++ include/IDebugger.h
@@ -28,6 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QString>
 #include <QtPlugin>
 #include <memory>
+#include <vector>
 
 class IDebugEvent;
 class IProcess;
