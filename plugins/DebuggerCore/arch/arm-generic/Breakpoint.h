eab57adf420a246b9f64ac25002e5215ac641e22
Evan Teran <evan.teran@gmail.com>
2018-08-23 15:05:26 -0400

    these need vector include too :-/

--- plugins/DebuggerCore/arch/arm-generic/Breakpoint.h
+++ plugins/DebuggerCore/arch/arm-generic/Breakpoint.h
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "IBreakpoint.h"
 #include "Util.h"
 #include <array>
+#include <vector>
 
 namespace DebuggerCorePlugin {
 
