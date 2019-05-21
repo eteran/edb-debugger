07b8cf2d2625ca92b4f37761668c6daa28fef70b
darkprof <dark_prof@mail.ru>
2019-05-21 09:34:32 +0500

    loadHeader func added.

--- plugins/FasLoader/FasCore.hpp
+++ plugins/FasLoader/FasCore.hpp
@@ -3,6 +3,7 @@
 #include "Header.hpp"
 
 #include <string>
+#include <fstream>
 
 class FasCore
 {
@@ -10,8 +11,13 @@ public:
   FasCore ();
   virtual ~FasCore () = default;
   void load ( const std::string& fileName );
+  void load ();
+  void open ();
+  void loadHeader ();
 
 private:
+  std::ifstream ifs;
+  std::string fileName;
   const uint32_t signature;
   Header header;
 };
