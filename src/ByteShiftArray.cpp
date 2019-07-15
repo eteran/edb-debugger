f5e0bce1c1d33e11062fd61a3bbad52ee18e8a8b
Evan Teran <evan.teran@gmail.com>
2019-07-15 14:44:25 -0400

    fix for issue #719

--- src/ByteShiftArray.cpp
+++ src/ByteShiftArray.cpp
@@ -17,6 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "ByteShiftArray.h"
+#include <climits>
 
 //------------------------------------------------------------------------------
 // Name: ByteShiftArray
