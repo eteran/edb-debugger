e1807bc89689afe76dc08a04b8458251b5ad1886
Evan Teran <evan.teran@gmail.com>
2020-12-14 20:52:14 -0500

    added needed include for assert

--- plugins/DebuggerCore/arch/x86-generic/Breakpoint.cpp
+++ plugins/DebuggerCore/arch/x86-generic/Breakpoint.cpp
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "IDebugger.h"
 #include "IProcess.h"
 #include "edb.h"
+#include <cassert>
 
 namespace DebuggerCorePlugin {
 
