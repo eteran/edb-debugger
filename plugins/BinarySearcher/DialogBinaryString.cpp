037bbedef207b2832637e86fa3a0083c25385672
Aaron Opfer <me@aaronopfer.com>
2022-06-23 09:24:56 -0500

    make binary string search read proc mem in chunks (#820)
    * make binary string search read proc mem in chunks
    
    Instead of trying to read an entire debuggee process region at
    once, read up to 4096 pages at a time. This should reduce memory
    issues when running against processes that allocate huge amounts of
    memory such as modern games.
    
    In addition, slightly optimize the performance if byte alignment is on.
    
    * change C-style cast to reinterpret_cast
    
    * error if search string is too big

--- plugins/BinarySearcher/DialogBinaryString.cpp
+++ plugins/BinarySearcher/DialogBinaryString.cpp
@@ -62,63 +62,78 @@ DialogBinaryString::DialogBinaryString(QWidget *parent, Qt::WindowFlags f)
  * @brief DialogBinaryString::doFind
  */
 void DialogBinaryString::doFind() {
-
+	// TODO: Algorithm here should be Boyer-Moore for better performance
 	const QByteArray b = ui.binaryString->value();
 
 	auto results = new DialogResults(this);
 
-	const int sz = b.size();
-	if (sz != 0) {
-		edb::v1::memory_regions().sync();
-		const QList<std::shared_ptr<IRegion>> regions = edb::v1::memory_regions().regions();
-		const size_t page_size                        = edb::v1::debugger_core->pageSize();
+	const edb::address_t align = ui.chkAlignment->isChecked() ? 1 << (ui.cmbAlignment->currentIndex() + 1) : 1;
+	const size_t sz = b.size();
+	edb::v1::memory_regions().sync();
+	const QList<std::shared_ptr<IRegion>> regions = edb::v1::memory_regions().regions();
+	const size_t page_size = edb::v1::debugger_core->pageSize();
 
-		int i = 0;
-		for (const std::shared_ptr<IRegion> &region : regions) {
-			const size_t region_size = region->size();
+	int i = 0;
 
-			// a short circut for speading things up
-			if (ui.chkSkipNoAccess->isChecked() && !region->accessible()) {
-				ui.progressBar->setValue(util::percentage(++i, regions.size()));
-				continue;
-			}
+	const size_t max_number_pages = 4096;  // how many pages our read chunks are.
 
-			const size_t page_count      = region_size / page_size;
-			const QVector<uint8_t> pages = edb::v1::read_pages(region->start(), page_count);
+	if (sz == 0) {
+		return;
+	}
+	if (sz > max_number_pages * page_size) {
+		QMessageBox::information(nullptr, tr("Input String Too Large"), tr("The search string is too large."));
+		return;
+	}
+
+	for (const std::shared_ptr<IRegion> &region : regions) {
+		const size_t region_size = region->size();
+
+		// a short circut for speading things up
+		if (ui.chkSkipNoAccess->isChecked() && !region->accessible()) {
+			ui.progressBar->setValue(util::percentage(++i, regions.size()));
+			continue;
+		}
+
+		const size_t page_count = region_size / page_size;
+		// Read out 4096 pages at a time, as some applications have huge regions
+		// and we will push against memory fragmentation if we mirror those regions.
+		// To prevent missing a needle in the haystack if it is split between read
+		// boundaries. increment current_page by only 4095.
+		for (size_t current_page = 0; current_page < page_count; current_page += max_number_pages - 1) {
+			const QVector<uint8_t> pages = edb::v1::read_pages(
+				region->start() + (current_page * page_size),
+				std::min(max_number_pages, page_count - current_page)
+			);
 
 			if (!pages.isEmpty()) {
 
-				const uint8_t *p               = &pages[0];
-				const uint8_t *const pages_end = &pages[0] + region_size - sz;
+				const uint8_t *p = &pages.constFirst();
+				const uint8_t *const pages_end = &pages.constLast() - sz;
 
 				while (p < pages_end) {
 					// compare values..
 					if (std::memcmp(p, b.constData(), sz) == 0) {
-						const edb::address_t addr  = p - &pages[0] + region->start();
-						const edb::address_t align = 1 << (ui.cmbAlignment->currentIndex() + 1);
-
-						if (!ui.chkAlignment->isChecked() || (addr % align) == 0) {
-							results->addResult(DialogResults::RegionType::Data, addr);
-						}
+						const edb::address_t addr = p - &pages[0] + region->start() + (current_page * page_size);
+						results->addResult(DialogResults::RegionType::Data, addr);
 					}
 
 					// update progress bar every 64KB
-					if ((uint64_t(p) & 0xFFFF) == 0) {
+					if ((reinterpret_cast<uint64_t>(p) & 0xFFFF) == 0) {
 						ui.progressBar->setValue(util::percentage(i, regions.size(), p - &pages[0], region_size));
 					}
 
-					++p;
+					p += align;
 				}
 			}
-			++i;
 		}
+		++i;
+	}
 
-		if (results->resultCount() == 0) {
-			QMessageBox::information(nullptr, tr("No Results"), tr("No Results were found!"));
-			delete results;
-		} else {
-			results->show();
-		}
+	if (results->resultCount() == 0) {
+		QMessageBox::information(nullptr, tr("No Results"), tr("No Results were found!"));
+		delete results;
+	} else {
+		results->show();
 	}
 }
 
