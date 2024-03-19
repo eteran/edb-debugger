fd818befb4d80ec25b76fd97191fe98c8877b359
Evan Teran <evan.teran@gmail.com>
2024-03-19 11:50:22 -0400

    formatting

--- plugins/DebuggerCore/unix/Unix.cpp
+++ plugins/DebuggerCore/unix/Unix.cpp
@@ -209,7 +209,7 @@ Status Unix::execute_process(const QString &path, const QString &cwd, const QLis
 
 		p = argv_pointers;
 		while (*p) {
-			delete[] *p++;
+			delete[] * p++;
 		}
 		delete[] argv_pointers;
 	}
