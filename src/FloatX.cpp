5ad3568f47d48f550e0f7748d8a277d0ced75c5e
Evan Teran <evan.teran@gmail.com>
2024-03-21 14:00:11 -0400

    adding in missing includes

--- src/FloatX.cpp
+++ src/FloatX.cpp
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "FloatX.h"
 #include "util/Float.h"
 
+#include <array>
 #include <iomanip>
 #include <sstream>
 
--- src/arch/x86-generic/ArchProcessor.cpp
+++ src/arch/x86-generic/ArchProcessor.cpp
@@ -38,6 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QWidget>
 #include <QXmlQuery>
 
+#include <array>
 #include <cctype>
 #include <climits>
 #include <cmath>
