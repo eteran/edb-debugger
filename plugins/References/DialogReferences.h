490fa9ce39da8549deabd45d574ba2da25de083a
Evan Teran <evan.teran@gmail.com>
2026-07-09 15:51:36 -0400

    References are now cancelable

--- plugins/References/DialogReferences.h
+++ plugins/References/DialogReferences.h
@@ -11,8 +11,14 @@
 #include "Types.h"
 #include "ui_DialogReferences.h"
 #include <QDialog>
+#include <QFutureWatcher>
+#include <QTimer>
+
+#include <atomic>
+#include <vector>
 
 class QListWidgetItem;
+class IProcess;
 
 namespace ReferencesPlugin {
 
@@ -30,14 +36,42 @@ Q_SIGNALS:
 	void updateProgress(int);
 
 private:
+	void reject() override;
 	void showEvent(QShowEvent *event) override;
 
 private:
-	void doFind();
+	struct RegionScan {
+		edb::address_t start = 0;
+		edb::address_t end   = 0;
+		bool accessible      = false;
+	};
+
+	struct ReferenceResult {
+		char type            = 0;
+		edb::address_t address = 0;
+	};
+
+	struct SearchResult {
+		std::vector<ReferenceResult> matches;
+		bool cancelled        = false;
+		bool allocationFailed = false;
+	};
+
+	void onFindClicked();
+	void onFindFinished();
+	void updateProgressBar();
+	void setSearchRunning(bool running);
+	SearchResult doFind(edb::address_t address, const IProcess *process, const std::vector<RegionScan> &regions, bool skipNoAccess);
 
 private:
 	Ui::DialogReferences ui;
 	QPushButton *buttonFind_ = nullptr;
+	QFutureWatcher<SearchResult> searchWatcher_;
+	QTimer progressTimer_;
+	std::atomic_bool cancelRequested_ = false;
+	std::atomic_size_t progressDone_  = 0;
+	std::atomic_size_t progressTotal_ = 0;
+	bool searchRunning_               = false;
 };
 
 }
